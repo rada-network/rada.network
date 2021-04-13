946a660fbd7f225a3de3747df1b59fce88a33e40
Hieu Nguyen <nnth83@gmail.com>
2021-04-13 15:12:03 +0700

    update: Project_Card

--- components/cards/Project.js
+++ components/cards/Project.js
@@ -2,45 +2,58 @@ import { RiArrowUpSFill } from "react-icons/ri";
 import { RiChat1Fill } from "react-icons/ri";
 
 
-export const Card = ({title, text, mediaUri, projectType, projectPlatform, projectDate, projectSubmitter, projectSubmitterImgUri, projectCommentsCounts, link, voteTotal}) => {
-  const content = 
+export const Card = ({title, text, mediaUri, projectType, projectPlatform, projectPlatformShort, projectDate, projectSubmitter, projectSubmitterImgUri, projectCommentsCounts, link, voteTotal}) => {
+  return (
     <div className="card group card-project flex-row items-center content-center">
       <div className="card-media project-icon">
         <img className="card-img project-icon_img" src={mediaUri} />
       </div> 
       <div className="card-body">
         <div className="card-body-header">
-          <div className="card-title">{title}</div>
+          <div className="card-title">
+           <a className="card-link" href={link}>
+              {title}
+            </a>
+          </div>
           <div className="metadata-wrapper project-metadata-wrapper">
-            <div class="metadata project-metadata_type">
-              <span class="metadata-value">{projectType}</span>
-            </div>
-            <div class="metadata project-metadata_platform">
-              <span class="metadata-value">{projectPlatform}</span>
-            </div>
+            <a href="#" className={`metadata badge badge-${projectType || ''} project-metadata_type`}>
+              <span className="metadata-value">{projectType}</span>
+            </a>
           </div>
         </div> 
         <div className="card-body-main">
           <div className="card-text">{text}</div>
         </div>
-        <div class="card-body-footer">
+
+        <div className="card-body-footer">
           <div className="metadata-wrapper project-metadata-wrapper">
-              <div class="metadata project-metadata_date">
-                <span class="icon"></span>
-                <span class="metadata-value">{projectDate}</span>
-              </div>
-              <div class="metadata project-metadata_submitter">
-                <div className="card-body-media project-submitter inline-block mr-1">
-                  <img className="card-img project-submitter_img" src={projectSubmitterImgUri} />
-                </div>
-                <span class="metadata-value">{projectSubmitter}</span>
-              </div>
-              <div class="metadata-divider"></div>
-              <div class="metadata project-comment_count">
-                <span class="icon mr-1"><RiChat1Fill /></span>
-                <span>{projectCommentsCounts}</span>
-              </div>
+
+            <a href="#" className={`metadata project-metadata_platform project-metadata_platform_${projectPlatformShort || ''} `}>
+              <img className="card-img" src="./node_modules/cryptocurrency-icons/svg/color/ada.svg" />
+              <span className="metadata-value">{projectPlatform}</span>
+            </a>
+
+            <div className="metadata-divider"></div>
+
+            <div className="metadata project-metadata_date">
+              <span className="icon"></span>
+              <span className="metadata-value">{projectDate}</span>
             </div>
+
+            <a href={link} className="metadata project-metadata_submitter hidden">
+              <span className="card-body-media project-submitter inline-block mr-1">
+                <img className="card-img project-submitter_img" src={projectSubmitterImgUri} />
+              </span>
+              <span className="metadata-value">{projectSubmitter}</span>
+            </a>
+
+            <div className="metadata-divider"></div>
+
+            <a href={link} className="metadata project-comment_count">
+              <span className="icon mr-1"><RiChat1Fill /></span>
+              <span>{projectCommentsCounts}</span>
+            </a>
+          </div>
         </div>
       </div>
       <div className="card-footer">
@@ -51,13 +64,6 @@ export const Card = ({title, text, mediaUri, projectType, projectPlatform, proje
           </span>
         </btn>
       </div>
-    </div>;
-
-  return link ? (
-    <a className="card-link" href={link}>
-      {content}
-    </a>
-  ) : (
-    content
+    </div>
   );
 };
