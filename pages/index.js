daf5c8382255fceb8a4e28f4eeaee034ed66e453
van010 <itmannavys@gmail.com>
2021-04-20 17:04:24 +0700

    update post query

--- pages/index.js
+++ pages/index.js
@@ -13,89 +13,97 @@ import {CategoryList} from '../components/card-layouts/CategoryList';
 import {IoChevronForwardSharp} from "react-icons/io5";
 import getClient from "../data/client";
 import postQuery from "../data/query/posts"
-import postNftQuery from "../data/query/postsNFT"
 import useSWR from "swr";
 
 
 const getData = async () => {
-    console.log("get data")
-    const client = getClient()
-    const posts = await client.query({
-        query: postQuery,
-        variables: {skip: 0, take: 4}
-    })
-    const postsNFT = await client.query({
-        query: postNftQuery,
-        variables: {skip: 0, take: 4, itemType: "nft"},
-    })
-    console.log("post",posts)
-    console.log("postNFt",postsNFT)
-    return {
-        posts: posts.data.itemFeed,
-        postsNFT: postsNFT.data.itemFeed,
-    }
+  console.log("get data")
+  const client = getClient()
+
+  const posts = await client.query({
+    query: postQuery,
+    variables: {skip: 0, take: 4, itemType: ""}
+  })
+
+  const postsNFT = await client.query({
+    query: postQuery,
+    variables: {skip: 0, take: 4, itemType: "nft"},
+  })
+
+  const postsDapp = await client.query({
+    query: postQuery,
+    variables: {skip: 0, take: 4, itemType: "dapp"}
+  })
+
+  console.log("Query is running...")
+
+  return {
+    posts: posts.data.itemFeed,
+    postsNFT: postsNFT.data.itemFeed,
+    postsDapp: postsDapp.data.itemFeed,
+  }
 }
 
 export default function Home(props) {
-    const {data} = useSWR('homepage', getData, {initialData: props})
-    return (
-        <Layout extraClass="page-home">
-            <Header/>
-            <ProjectsList
-                grid="2"
-                gap="2"
-                title="Popular Projects Today"
-                posts={data.posts}
-            />
-            <CategoryList
-                extraClass="category-list"
-                grid="5"
-                gap="5"
-                title="Project Categories"
-                cta="View all"
-            />
-            <ProjectsList
-                grid="2"
-                gap="2"
-                title="NFTs that you cannot missed"
-                posts={data.postsNFT}
-            />
-            <ProjectsList
-                grid="2"
-                gap="2"
-                title="Most Active Dapps in a Week"
-                posts={data.posts}
-            />
-            <TopUsersList
-                grid="5"
-                gap="5"
-            />
-            <ProjectsList
-                grid="1"
-                gap="2"
-                title="New Projects Today"
-                posts={data.posts}
-            />
-            <PromoList
-                extraClass="promo-list"
-                grid="3"
-                gap="5"
-            />
-            <ProjectsList
-                grid="1"
-                gap="2"
-                title="All Projects"
-                cta="Sorted by"
-                posts={data.posts}
-            />
-        </Layout>
-    )
+  const {data} = useSWR('homepage', getData, {initialData: props})
+  return (
+    <Layout extraClass="page-home">
+      <Header/>
+      <ProjectsList
+        grid="2"
+        gap="2"
+        title="Popular Projects Today"
+        posts={data.posts}
+      />
+      <CategoryList
+        extraClass="category-list"
+        grid="5"
+        gap="5"
+        title="Project Categories"
+        cta="View all"
+      />
+      <ProjectsList
+        grid="2"
+        gap="2"
+        title="NFTs that you cannot missed"
+        posts={data.postsNFT}
+      />
+      <ProjectsList
+        grid="2"
+        gap="2"
+        title="Most Active Dapps in a Week"
+        posts={data.postsDapp}
+      />
+      <TopUsersList
+        grid="5"
+        gap="5"
+      />
+      <ProjectsList
+        grid="1"
+        gap="2"
+        title="New Projects Today"
+        posts={data.posts}
+      />
+      <PromoList
+        extraClass="promo-list"
+        grid="3"
+        gap="5"
+      />
+      <ProjectsList
+        grid="1"
+        gap="2"
+        title="All Projects"
+        cta="Sorted by"
+        posts={data.posts}
+      />
+    </Layout>
+  )
 }
 
 export async function getStaticProps() {
-    const props = await getData();
-    return {
-        props,
-        revalidate: 1
-    }
+  const props = await getData();
+  return {
+    props,
+    revalidate: 1
+  }
 }
