9717f9ad71977d11a4817685924f0097075176a0
Hieu Nguyen <nnth83@gmail.com>
2021-05-21 17:16:29 +0700

    - add: concepts folder

--- pages/concepts/project_details.js
+++ pages/concepts/project_details.js
@@ -5,9 +5,9 @@ import dynamic from "next/dynamic";
 import TextareaAutosize from 'react-textarea-autosize';
 
 // Components
-import {Layout} from '../components/page-layouts/OneColumn';
-import {Header} from '../components/Header';
-import {ProjectsList} from '../components/card-layouts/ProjectsList';
+import {Layout} from '../../components/page-layouts/OneColumn';
+import {Header} from '../../components/Header';
+import {ProjectsList} from '../../components/card-layouts/ProjectsList';
 
 //ReactIcons
 import { IoChevronForwardSharp } from "react-icons/io5";
