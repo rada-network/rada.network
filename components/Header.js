d40bb56deb463d113ff670ef2ecc2bf0c1bbf4a4
Hieu Nguyen <nnth83@gmail.com>
2021-05-26 10:40:39 +0700

    - add hero

--- components/Header.js
+++ components/Header.js
@@ -9,18 +9,21 @@ export const Header = () => {
 
   return (
     <div className="header">
-      <div className="container py-12 lg:py-16">
+      <div className="container">
 
-        <div className="flex flex-col max-w-screen-sm">
-          <div className="">
-            <h2 className="mb-2 text-4xl leading-snug font-medium text-white">
+        <div className="flex flex-wrap">
+          <div className="py-12 lg:py-16 flex flex-col max-w-screen-sm lg:w-1/2">
+            <h2 className="mb-2 text-2xl leading-10 lg:text-4xl lg:leading-snug font-medium text-white">
               <span className="relative inline-block">
                 Discover all the latest Cardano News, Feeds, Discussions, Projects and more... 
               </span>
             </h2>
-            <p className="text-base text-lg font-light text-white text-opacity-70">
+            <p className="text-base lg:text-lg font-light text-white text-opacity-70">
               and always be in the loop!
             </p>
+          </div>
+          <div className="lg:w-1/2">
+            <img className="block w-auto h-auto" src="/images/hero.png" alt="dhunt.io" />
           </div>
           {/* <div className="mt-4">
             <a href="/" className="h-12 px-6 font-medium tracking-wide rounded btn btn-secondary">
