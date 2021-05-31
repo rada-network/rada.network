3e50db696cc229cd3d21714213a08d531bdb6563
Hieu Nguyen <nnth83@gmail.com>
2021-05-31 14:57:18 +0700

    update wallet

--- components/Wallet.js
+++ components/Wallet.js
@@ -1,11 +1,12 @@
 import { useWallet, ConnectionRejectedError } from 'use-wallet'
-import { Dialog, Transition } from "@headlessui/react"
 import { BiWallet } from "react-icons/bi";
 import { GiWallet } from "react-icons/gi";
 import { Fragment, useState, useRef } from "react"
 import { observer, inject } from "mobx-react"
 import { useStore } from '../lib/useStore'
 
+import { Dialog, Transition } from "@headlessui/react"
+import ReactTooltip from 'react-tooltip';
 
 const WalletContent = ({wallet, closeModal, open}) => {
   const cancelButtonRef = useRef();
@@ -51,63 +52,95 @@ const WalletContent = ({wallet, closeModal, open}) => {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-0"
           >
-            <div className="inline-block w-full max-w-sm overflow-hidden text-left align-middle transition-all transform bg-white shadow-2xl rounded">
+            <div className="inline-block w-full max-w-2xl overflow-hidden text-left align-middle transition-all transform bg-white shadow-2xl rounded">
 
-              <button type="button" className="btn text-gray-900 text-opacity-10 justify-center absolute top-2 right-2 rounded-full w-px-24 h-px-24 hover:bg-red-100 hover:text-red-700" onClick={closeModal}>
-                <i className="far fa-times text-sm"></i>
-              </button>
+              <div className="flex">
 
-              <Dialog.Title
-                as="div"
-                className="dialog-header py-5 px-8"
-              >
-                <h3 className="text-lg font-semibold">Connect to your Wallet</h3>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-900 text-opacity-60">
-                  Select a Wallet Connector to connect with your wallet
-                  </p>
+                {/* Dialog Header */}
+                <div className="flex w-5/12 bg-gradient-to-br from-purple-700 to-purple-500 text-white">
+                  <Dialog.Title
+                    as="div"
+                    className="dialog-header flex flex-col py-6 px-8"
+                  >
+                    <h3 className="text-xl font-semibold">Connect your wallet</h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-white text-opacity-50">
+                        Connect with one of our available wallet providers.
+                      </p>
+                    </div>
+                  </Dialog.Title>
                 </div>
-              </Dialog.Title>
 
-              <div className="dialog-body py-8 px-8 bg-gradient-to-tr from-gray-50 to-gray-100 bg-gray-50">
+                {/* Dialog Body */}
+                <div className="flex w-7/12">
 
-                <ul>
-                  <li>
-                    <a className="group btn rounded-md bg-white border border-black border-opacity-10 py-3 px-5 my-2 w-full hover:bg-primary-100 hover:border-primary-500 hover:shadow-md" onClick={() => wallet.connect()}>
-                      <span className="icon w-px-24 h-px-24 mr-4">
-                        <img className="block w-auto" src="/images/icons/metamask-24.png" alt="Metamask" />
-                      </span>
-                      <span className="flex-1">Metamask</span>
-                      <i className="fal fa-long-arrow-right transform group-hover:translate-x-1 transition-transform"/>
-                    </a>
-                  </li>
-                  <li>
-                    <a className="group btn rounded-md bg-white border border-black border-opacity-10 py-3 px-5 my-2 w-full hover:bg-primary-100  hover:border-primary-500 hover:shadow-md" onClick={() => wallet.connect('walletconnect')}>
-                      <span className="icon w-px-24 h-px-24 mr-4">
-                        <img className="block w-auto" src="/images/icons/walletconnect-24.png" alt="WalletConnect" />
-                      </span>
-                      <span className="flex-1">WalletConnect</span>
-                      <i className="fal fa-long-arrow-right transform group-hover:translate-x-1 transition-transform"/>
-                    </a>
-                  </li>
-                  <li>
-                    <a className="group btn rounded-md bg-white border border-black border-opacity-10 py-3 px-5 my-2 w-full hover:bg-primary-100  hover:border-primary-500 hover:shadow-md" onClick={() => wallet.connect('walletlink')}>
-                      <span className="icon w-px-24 h-px-24 mr-4">
-                        <img className="block w-auto rounded-md" src="/images/icons/walletlink-24.png" alt="WalletLink" />
-                      </span>
-                      <span className="flex-1">WalletLink</span>
-                      <i className="fal fa-long-arrow-right transform group-hover:translate-x-1 transition-transform"/>
-                    </a>
-                  </li>
-                </ul>                    
+                  <button type="button" className="btn text-gray-900 text-opacity-10 justify-center absolute top-2 right-2 rounded-full w-px-24 h-px-24 hover:bg-red-100 hover:text-red-700" onClick={closeModal}>
+                    <i className="far fa-times text-sm"></i>
+                  </button>
+
+                  <div className="dialog-body py-8 px-8 bg-gradient-to-tr from-gray-50 to-gray-100">
+
+                    <ul>
+                      <li>
+                        <a className="group btn rounded-md bg-white border border-black border-opacity-10 py-3 px-5 mb-4 w-full hover:bg-primary-100 hover:border-primary-500 hover:shadow-md" onClick={() => wallet.connect()}>
+                          <span className="icon w-px-24 h-px-24 mr-4">
+                            <img className="block w-auto" src="/images/icons/metamask-24.png" alt="Metamask" />
+                          </span>
+                          <div className="flex flex-col flex-1 mr-4">
+                            <span className="text-base font-semibold">Metamask</span>
+                            <span className="text-xs text-gray-900 text-opacity-50 mt-1 font-normal">One of the most secure wallets with great flexibility</span>
+                          </div>
+                          <i className="fal fa-long-arrow-right transform group-hover:translate-x-1 transition-transform"/>
+                        </a>
+                      </li>
+                      <li>
+                        <a className="group btn rounded-md bg-white border border-black border-opacity-10 py-3 px-5 mb-4 w-full hover:bg-primary-100  hover:border-primary-500 hover:shadow-md" onClick={() => wallet.connect('walletconnect')}>
+                          <span className="icon w-px-24 h-px-24 mr-4">
+                            <img className="block w-auto" src="/images/icons/walletconnect-24.png" alt="WalletConnect" />
+                          </span>
+                          <div className="flex flex-col flex-1 mr-4">
+                            <span className="text-base font-semibold">WalletConnect</span>
+                            <span className="text-xs text-gray-900 text-opacity-50 mt-1 font-normal">Connect with <b>Rainbow</b>, <b>Trust</b>, <b>Argent</b> and more</span>
+                          </div>
+                          <i className="fal fa-long-arrow-right transform group-hover:translate-x-1 transition-transform"/>
+                        </a>
+                      </li>
+                      <li>
+                        <a className="group btn rounded-md bg-white border border-black border-opacity-10 py-3 px-5 mb-8 w-full hover:bg-primary-100  hover:border-primary-500 hover:shadow-md" onClick={() => wallet.connect('walletlink')}>
+                          <span className="icon w-px-24 h-px-24 mr-4">
+                            <img className="block w-auto rounded-md" src="/images/icons/walletlink-24.png" alt="WalletLink" />
+                          </span>
+                          <div className="flex flex-col flex-1 mr-4">
+                            <span className="text-base font-semibold">WalletLink</span>
+                            <span className="text-xs text-gray-900 text-opacity-50 mt-1 font-normal">Connect with <b>Coinbase</b> wallet</span>
+                          </div>
+                          <i className="fal fa-long-arrow-right transform group-hover:translate-x-1 transition-transform"/>
+                        </a>
+                      </li>
+                    </ul>    
+
+                    <div className="mt-auto">
+                      <p className="text-xs text-gray-900 text-opacity-50">
+                        We do not own your private keys and cannot access your funds without your confirmation.
+                      </p>
+                    </div>
+
+                  </div>
+                
+                </div>
+              
               </div>
 
             </div>
           </Transition.Child>
 
         </div>
+
       </Dialog>
+
+
     </Transition>  
+
   )
 }
 
