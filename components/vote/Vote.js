7a260ed4f0d4a9c9ca601544f9cd396e4355ee29
tan <tanz.cntt@gmail.com>
2021-06-08 15:51:24 +0700

    change vote module

--- components/vote/Vote.js
+++ components/vote/Vote.js
@@ -20,11 +20,15 @@ export const Vote = observer(({itemId, page,voteStore}) => {
     totalVote = vote[0].totalVote
     isVote = vote[0].isVoted
   }
-  const toggleVote = async () => {
+  const [rqToggleVote,setRqToggleVote] = useState(false)
+  const toggleVote = async (e) => {
     if (!walletAddress) {
       return store.wallet.showConnect(true)
     }
-
+    if (rqToggleVote){
+      return false;
+    }
+    setRqToggleVote(true)
     const res = await client.mutate({
       mutation : tgVote,
       variables : {itemId: itemId, walletAddress}
@@ -34,7 +38,7 @@ export const Vote = observer(({itemId, page,voteStore}) => {
       isVoted : res.data.toggleVote.isVoted,
       totalVote : res.data.toggleVote.totalVote
     })
-    console.log(res)
+    setRqToggleVote(false)
   }
 
   if (page === "detail" ) return (
@@ -43,13 +47,13 @@ export const Vote = observer(({itemId, page,voteStore}) => {
           onClick={toggleVote}>
       <span className="-mb-0.5 -ml-1 text-2xl icon"><RiArrowUpSFill /></span>
       <span className="ml-1 btn-project-vote_total whitespace-nowrap">
-        <span className="inline-block font-medium">Upvote</span>
+        <span className="inline-block font-medium">{isVote? "Upvoted" : "Upvote"}</span>
         <strong className="inline-block ml-2 font-bold">{`${totalVote}`}</strong>
       </span>
       </button>
     )
    return (
-    <button className={`btn btn-project-vote 
+    <button title={isVote? "Upvoted" : "Upvote"} className={`btn btn-project-vote 
     ${!isVote ? "" : "active"}`}
         onClick={toggleVote}>
       <span className="text-xl -mb-0.5 transition-none icon"><RiArrowUpSFill /></span>
