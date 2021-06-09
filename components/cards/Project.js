51b18cdc0bb9d4a2566ee01b0c755362680f6db1
Hieu Nguyen <nnth83@gmail.com>
2021-06-09 15:04:37 +0700

    Update Project card

--- components/cards/Project.js
+++ components/cards/Project.js
@@ -49,20 +49,6 @@ export const Card = ({
       <div className={`${styles.card_body}`}>
 
         <div className={`${styles.card_body_header}`}>
-
-          <div className="metadata-wrapper project-metadata-wrapper">
-
-            <ItemTypeIcon projectType={projectType} />
-
-            <NetworkIcon projectPlatform={projectPlatform} projectPlatformShort={projectPlatformShort} projectWebsiteUri={projectWebsiteUri} badge={false}/>
-            { projectDate && 
-            <div className="metadata project-metadata_date">
-              {/*<span className="metadata-value">{projectDate.split(('T'))[0]}</span>*/}
-              <span className="metadata-value" title={`${titleTime}`}>{postDate}</span>
-            </div> }
-
-          </div>
-
           <div className={`${styles.card_title}`}>
             <Link href={`/item/${post.id}`}>
               <a className="card-link">{projectTitle}</a>
@@ -71,19 +57,29 @@ export const Card = ({
         </div>
 
         <div className={`${styles.card_body_main}`}>
-
           <div className={`${styles.card_text}`} dangerouslySetInnerHTML={{__html: projectTextShort}} />
-
         </div>
 
         <div className={`${styles.card_body_footer}`}>
           <div className="metadata-wrapper project-metadata-wrapper">
+
             <a href={link} className="metadata project-comment_count">
               <span className="icon mr-1 text-base opacity-40">
                 <svg viewBox="0 0 24 24" aria-hidden="true" width="1em" heigh="1em"><g><path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path></g></svg>
               </span>
               <span>{projectCommentsCounts}</span>
             </a>
+
+            <ItemTypeIcon projectType={projectType} />
+
+            <NetworkIcon projectPlatform={projectPlatform} projectPlatformShort={projectPlatformShort} projectWebsiteUri={projectWebsiteUri} badge={false}/>
+
+            { projectDate && 
+            <div className="metadata project-metadata_date">
+              {/*<span className="metadata-value">{projectDate.split(('T'))[0]}</span>*/}
+              <span className="metadata-value" title={`${titleTime}`}>{postDate}</span>
+            </div> }
+
           </div>
         </div>
 
--- components/vote/Vote.js
+++ components/vote/Vote.js
@@ -45,20 +45,20 @@ export const Vote = observer(({itemId, page,voteStore}) => {
     <button className={`btn btn-project-vote
     ${!isVote ? "" : "active"}`}
           onClick={toggleVote}>
-      <span className="-mb-0.5 -ml-1 text-2xl icon"><RiArrowUpSFill /></span>
-      <span className="ml-1 btn-project-vote_total whitespace-nowrap">
+      <span className="icon -mb-0.5 -ml-1 text-2xl"><RiArrowUpSFill /></span>
+      <span className="btn-project-vote_total ml-1 whitespace-nowrap">
         <span className="inline-block font-medium">{isVote? "Upvoted" : "Upvote"}</span>
         <strong className="inline-block ml-2 font-bold">{`${totalVote}`}</strong>
       </span>
       </button>
     )
    return (
-    <button title={isVote? "Upvoted" : "Upvote"} className={`btn btn-project-vote 
+    <button title={isVote? "Upvoted" : "Upvote"} className={`btn btn-project-vote_sm flex flex-col  
     ${!isVote ? "" : "active"}`}
         onClick={toggleVote}>
-      <span className="text-xl -mb-0.5 transition-none icon"><RiArrowUpSFill /></span>
-      <span className="ml-1 btn-project-vote_total whitespace-nowrap">
-        <strong className="inline-block text-xs">{`${totalVote}`}</strong>
+      <span className="icon text-xl"><RiArrowUpSFill /></span>
+      <span className="btn-project-vote_total whitespace-nowrap">
+        <strong className="inline-block text-sm">{`${totalVote}`}</strong>
       </span>
     </button>
    )
