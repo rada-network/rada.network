edb97627276dd7687b6c2c21790a881ada10b23a
Khanh Le <khanhle@Khanhs-iMac.local>
2021-06-11 15:56:11 +0700

    add Pricing Widget

---
+++ components/chart/LineChart.js
@@ -0,0 +1,206 @@
+import {useState} from "react"
+
+
+export default function LineChart(props) {
+
+  let {data, svgHeight, svgWidth, color, pointRadius, onChartHover, showLabels, xLabelSize, yLabelSize} = {
+    data: [],
+    color: '#2196F3',
+    pointRadius: 5,
+    svgHeight: 300,
+    svgWidth: 500,
+    xLabelSize: 20,
+    yLabelSize: 80,
+    showLabels: true,
+    onChartHover: () => '',
+    ...props
+  }
+
+  if (!showLabels) {
+    xLabelSize = yLabelSize = 0
+  }
+
+  if (!data?.length) return 'Loading...'
+
+  const [hoverLoc, setHoverLoc] = useState(null)
+  const [activePoint, setActivePoint] = useState(null)
+
+  // GET X & Y || MAX & MIN
+  const getX = () => {
+    return {
+      min: data[0].x,
+      max: data[data.length - 1].x
+    }
+  }
+
+  const getY = () => {
+    return {
+      min: data.reduce((min, p) => p.y < min ? p.y : min, data[0].y),
+      max: data.reduce((max, p) => p.y > max ? p.y : max, data[0].y)
+    }
+  }
+
+  // GET SVG COORDINATES
+  const getSvgX = (x) => {
+    return yLabelSize + (x / getX().max * (svgWidth - yLabelSize));
+  }
+
+  const getSvgY = (y) => {
+    const gY = getY();
+    return ((svgHeight - xLabelSize) * gY.max - (svgHeight - xLabelSize) * y) / (gY.max - gY.min);
+  }
+
+  // BUILD SVG PATH
+  const Path = () => {
+    let pathD = "M " + getSvgX(data[0].x) + " " + getSvgY(data[0].y) + " ";
+
+    pathD += data.map((point, i) => {
+      return "L " + getSvgX(point.x) + " " + getSvgY(point.y) + " ";
+    }).join("");
+
+    return (
+      <path className="linechart_path" d={pathD} style={{stroke: color}} />
+    );
+  }
+
+  // BUILD SHADED AREA
+  const Area = () => {
+    let pathD = "M " + getSvgX(data[0].x) + " " + getSvgY(data[0].y) + " ";
+
+    pathD += data.map((point, i) => {
+      return "L " + getSvgX(point.x) + " " + getSvgY(point.y) + " ";
+    }).join("");
+
+    const x = getX();
+    const y = getY();
+    pathD += "L " + getSvgX(x.max) + " " + getSvgY(y.min) + " "
+    + "L " + getSvgX(x.min) + " " + getSvgY(y.min) + " ";
+
+    return <path className="linechart_area" d={pathD} />
+  }
+
+  // BUILD GRID AXIS
+  const Axis = () => {
+    const x = getX();
+    const y = getY();
+
+    return (
+      <g className="linechart_axis">
+        <line
+          x1={getSvgX(x.min) - yLabelSize} y1={getSvgY(y.min)}
+          x2={getSvgX(x.max)} y2={getSvgY(y.min)}
+          strokeDasharray="5" />
+        <line
+          x1={getSvgX(x.min) - yLabelSize} y1={getSvgY(y.max)}
+          x2={getSvgX(x.max)} y2={getSvgY(y.max)}
+          strokeDasharray="5" />
+      </g>
+    );
+  }
+  const Labels = () => {
+    if (!showLabels) return ''
+
+    const padding = 5;
+    const sd = data[0].d
+    const ed = data[data.length - 1].d
+    return(
+      <g className="linechart_label">
+        {/* Y AXIS LABELS */}
+        <text transform={`translate(${yLabelSize/2}, 20)`} textAnchor="middle">
+          {getY().max.toLocaleString('us-EN',{ style: 'currency', currency: 'USD' })}
+        </text>
+        <text transform={`translate(${yLabelSize/2}, ${svgHeight - xLabelSize - padding})`} textAnchor="middle">
+          {getY().min.toLocaleString('us-EN',{ style: 'currency', currency: 'USD' })}
+        </text>
+        {/* X AXIS LABELS */}
+        <text transform={`translate(${yLabelSize}, ${svgHeight})`} textAnchor="start">
+          { sd }
+        </text>
+        <text transform={`translate(${svgWidth}, ${svgHeight})`} textAnchor="end">
+          { ed }
+        </text>
+      </g>
+    )
+  }
+
+  // FIND CLOSEST POINT TO MOUSE
+  const getCoords = (e) => {
+    const svgLocation = document.getElementsByClassName("linechart")[0].getBoundingClientRect();
+    const adjustment = (svgLocation.width - svgWidth) / 2; //takes padding into consideration
+    const relativeLoc = e.clientX - svgLocation.left - adjustment;
+
+    let svgData = [];
+    data.map((point, i) => {
+      svgData.push({
+        svgX: getSvgX(point.x),
+        svgY: getSvgY(point.y),
+        d: point.d,
+        p: point.p
+      });
+    });
+
+    let closestPoint = {};
+    for(let i = 0, c = 500; i < svgData.length; i++){
+      if ( Math.abs(svgData[i].svgX - hoverLoc) <= c ){
+        c = Math.abs(svgData[i].svgX - hoverLoc);
+        closestPoint = svgData[i];
+      }
+    }
+
+    if(relativeLoc - yLabelSize < 0){
+      stopHover();
+    } else {
+      setHoverLoc(relativeLoc)
+      setActivePoint(closestPoint)
+      onChartHover(relativeLoc, closestPoint);
+    }
+  }
+
+  // STOP HOVER
+  const stopHover = () => {
+    setHoverLoc(null)
+    setActivePoint(null)
+    onChartHover(null, null);
+  }
+
+  // MAKE ACTIVE POINT
+  const ActivePoint = () => {
+    if (!hoverLoc) return ''
+    return (
+      <circle
+        className='linechart_point'
+        style={{stroke: color}}
+        r={pointRadius}
+        cx={activePoint.svgX}
+        cy={activePoint.svgY}
+      />
+    );
+  }
+
+  // MAKE HOVER LINE
+  const Line = () => {
+    if (!hoverLoc) return ''
+    return (
+      <line className='hoverLine'
+        x1={hoverLoc} y1={-8}
+        x2={hoverLoc} y2={svgHeight - xLabelSize} />
+    )
+  }
+
+
+  return (
+    <svg  width={svgWidth} height={svgHeight} viewBox={`0 0 ${svgWidth} ${svgHeight}`} className={'linechart'}
+          onMouseLeave={ () => stopHover() }
+          onMouseMove={ (e) => getCoords(e) } >
+      <g>
+        <Axis />
+        <Path />
+        <Area />
+        <Labels />
+        <Line />
+        <ActivePoint />
+      </g>
+    </svg>
+  );  
+
+}
--- components/widgets/WidgetPricing.js
+++ components/widgets/WidgetPricing.js
@@ -1,7 +1,55 @@
 import styles from '../../styles/modules/Widget.module.css'
 import stylesPricing from '../../styles/modules/Widget.pricing.module.css'
 
+import {useEffect, useState} from 'react'
+import fetchJson from "../../lib/fetchJson"
+import LineChart from "../chart/LineChart"
+
+
 export const WidgetPricing = ({title, text, footer, projectPlatformShort}) => {
+
+  const [duration, setDuration] = useState(24)
+  const [data, setData] = useState([])
+  const endDate = new Date()
+  const startDate = new Date(endDate.getTime() - duration * 60 * 60 * 1000)
+  const fdate = d => d.toISOString().substr(0, 16)
+  const url = `https://production.api.coindesk.com/v2/price/values/ADA?start_date=${fdate(startDate)}&end_date=${fdate(endDate)}&ohlc=false`
+  
+  useEffect(() => {
+    fetchJson(url).then(res => {
+      const sortedData = [];
+      let count = 0;
+      res.data.entries.forEach((c, idx) => {
+        sortedData.push({
+          d: '', //new Date(c[0]).toISOString(),
+          p: c[1].toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }),
+          x: count, //previous days
+          y: c[1] // numerical price
+        });
+        count++;
+      })
+
+      setData(sortedData)
+    })
+  }, [duration])
+
+  const Duration = () => {
+    const times = [
+      ['1h', 1],
+      ['1d', 24],
+      ['1w', 24*7],
+      ['1m', 24*7*30],
+      ['3m', 24*7*90],
+      ['1y', 24*7*365],
+    ]
+
+    return (
+      <div className="times">
+        { times.map(t => (<span className={`time${t[1]==duration?' active':''}`} onClick={e => setDuration(t[1])}>{t[0]}</span>)) }
+      </div>
+    )
+  }
+
   return (
 
     <div className={`group ${styles.widget}`}>
@@ -29,7 +77,8 @@ export const WidgetPricing = ({title, text, footer, projectPlatformShort}) => {
 
         {/* Pricing Chart */}
         <div className={`${stylesPricing.chart}`}>
-          
+          <Duration />
+          <LineChart data={data} onChartHover={ (a,b) => '' } showLabels={false} svgWidth={400} /> 
         </div>
 
 
