e6125f66fe4f4a8fe81accce9abd33e9242ec524
tan <tanz.cntt@gmail.com>
2021-06-22 17:57:13 +0700

    add event data

--- components/widgets/WidgetEvents.js
+++ components/widgets/WidgetEvents.js
@@ -8,6 +8,7 @@ import {useEffect, useState} from "react";
 import {getCardanoEvents} from "../../data/query/getEvents";
 import {uuid} from "@walletconnect/utils";
 
+
 const EventKeyword = ({word}) =>{
   const cWord = word.trim().toLowerCase()
   if (cWord === ""){
@@ -15,7 +16,7 @@ const EventKeyword = ({word}) =>{
   }
   word = word.charAt(0).toUpperCase() + word.slice(1)
   return (
-    <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-important">
+    <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-launchpad">
       {word}
     </span>
   )
@@ -23,25 +24,24 @@ const EventKeyword = ({word}) =>{
 
 const EventItem = ({item}) => {
   const startDate = new Date(item.startedAt)
+  const endDate = new Date(item.endedAt)
   const currentDate = new Date()
-  const day = Math.round((startDate.getTime() - currentDate.getTime())/(1000*24*60*60))
+  const day = Math.round((startDate.getTime() - currentDate.getTime()) / (1000 * 24 * 60 * 60))
+  const endDay = Math.round((endDate.getTime() - currentDate.getTime()) / (1000 * 24 * 60 * 60))
+
+
 
   const url = item.website ? item.website : item.twitter
   const keywords = item.keywords !== null ? item.keywords.split(",") : []
   return (
-    <a href={url} className={`${styles.widget_list__link}`} rel={'nofollow'} target="_blank">
-      <div className={`group ${styles.widget_list__item}`}>
-        <div className={`${styles.widget_list}`}>
-
-          {/* Even Item */}
-          <Link href="#" target="_blank">
-          <div className={`group ${styles.widget_list__item} ${styles.widget_list__link}`}>
+    <a href={url} target="_blank" rel={'nofollow'}>
+      <div className={`group ${styles.widget_list__item} ${styles.widget_list__link}`}>
 
         <div className={`${stylesEvent.title}`}>
           <a className={`${styles.widget_list__link}`} href="#" target="_blank">
-                <span>{item.title}</span>
-          <span className="icon ml-2 -mb-0.5 icon ico-external-link"><RiExternalLinkLine/></span>
-              </a>
+            <span>Cardano First Ever Launchpad</span>
+            <span className="icon ml-2 -mb-0.5 icon ico-external-link"><RiExternalLinkLine /></span>
+          </a>
         </div>
         <div className={`${stylesEvent.info_wrapper}`}>
           {keywords.map(function (word) {
@@ -49,26 +49,36 @@ const EventItem = ({item}) => {
               <EventKeyword key={uuid()} word={word}/>
             )
           })}
-          {day > 0 ?
-            <span className={`${stylesEvent.info} ${stylesEvent.info_date}`} title="00:00, 30, Sep 2021">
-                  <strong>{day}d</strong> to go
+          {endDay < 0 ?
+            <>
+              <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-expired">
+                Expired
               </span>
-            : ""
+              <span className={`${stylesEvent.info} ${stylesEvent.info_date}`} title={item.startedAt}>
+                  <strong>{Math.abs(endDay)}d</strong> ago
+              </span>
+            </>
+            :
+            (day > 0) ?
+            <span className={`${stylesEvent.info} ${stylesEvent.info_date}`} title={item.startedAt}>
+                <strong>{day}d</strong> to go
+              </span>
+              :  <span className={`${stylesEvent.info} ${stylesEvent.info_date}`} title={item.startedAt}>
+                Ongoing
+              </span>
           }
+
         </div>
         <div className={`${stylesEvent.text}`}>
           {item.description}
         </div>
 
       </div>
-    </Link>
+    </a>
   )
 }
 
 export const WidgetEvents = ({title, widgetIcon, widgetIconColor}) => {
-          {/* Even Item */}
-          <Link href="#" target="_blank">
-          <div className={`group ${styles.widget_list__item} ${styles.widget_list__link}`}>
 
   const [listEvents, setListEvents] = useState([])
   const [skip, setSkip] = useState(1)
@@ -87,103 +97,45 @@ export const WidgetEvents = ({title, widgetIcon, widgetIconColor}) => {
     })
   }, [])
   const _list = listEvents.slice(0, skip * take)
+
   return (
+
     <div className={`${styles.widget}`}>
 
-      {title &&
+      { title &&
       <div className={`${styles.widget_header}`}>
         <div className={`${styles.widget_title}`}>{title}</div>
         <span className={`${styles.widget_icon}`}>
           <i className={`fad fa-${widgetIcon || ''} text-${widgetIconColor || 'gray-400'} ${styles.widget_icon_fa}`}/>
-            <div className={`${stylesEvent.title}`}>
-              <a className={`${styles.widget_list__link}`} href="#" target="_blank">
-                <span>Cardano First Ever Airdrop</span>
-                <span className="icon ml-2 -mb-0.5 icon ico-external-link"><RiExternalLinkLine /></span>
-              </a>
-            </div>
-            <div className={`${stylesEvent.info_wrapper}`}>
-              <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-airdrop">
-                Airdrop
         </span>
-      </div>}
+      </div> }
 
       <div className={`${styles.widget_body_p0}`}>
-          </div>
-          </Link>
 
         <div className={`${styles.widget_list}`}>
+
+          {/* Even Item */}
           {_list.map(function (item) {
             return (
               <EventItem key={item.id} item={item}/>
             )
           })}
-          {/* Even Item */}
-          <Link href="#" target="_blank">
-          <div className={`group ${styles.widget_list__item} ${styles.widget_list__link}`}>
 
-            <div className={`${stylesEvent.title}`}>
-              <a className={`${styles.widget_list__link}`} href="#" target="_blank">
-                <span>Cardano First Ever Launchpad</span>
-                <span className="icon ml-2 -mb-0.5 icon ico-external-link"><RiExternalLinkLine /></span>
-              </a>
-            </div>
-            <div className={`${stylesEvent.info_wrapper}`}>
-              <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-launchpad">
-                Launchpad
-              </span>
-              <span className={`${stylesEvent.info} ${stylesEvent.info_date}`} title="19:00, 30, Sep 2021">
-                <strong>5d 23h</strong> to go
-              </span>
-            </div>
-            <div className={`${stylesEvent.text}`}>
-              "Follow me down to the valley below You know Moonlight is bleeding From out of your soul...
         </div>
 
       </div>
-          </Link>
-
-          {/* Even Item */}
-          <Link href="#" target="_blank">
-          <div className={`group ${styles.widget_list__item} ${styles.widget_list__link}`}>
 
       {(skip * take) < listEvents.length ?
-            <div className={`${stylesEvent.title}`}>
-              <a className={`${styles.widget_list__link}`} href="#" target="_blank">
-                <span>Cardano First Ever Launchpad</span>
-                <span className="icon ml-2 -mb-0.5 icon ico-external-link"><RiExternalLinkLine /></span>
-              </a>
-            </div>
-            <div className={`${stylesEvent.info_wrapper}`}>
-              <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-launchpad">
-                Launchpad
-              </span>
-              <span className={`${stylesEvent.info} ${stylesEvent.info_type}`} type="event-expired">
-                Expired
-              </span>
-              <span className={`${stylesEvent.info} ${stylesEvent.info_date}`} title="19:00, 30, Sep 2021">
-                <strong>25d</strong> ago
-              </span>
-            </div>
-            <div className={`${stylesEvent.text}`}>
-              "Follow me down to the valley below You know Moonlight is bleeding From out of your soul...
-            </div>
-
-          </div>
-          </Link>
-
-        </div>
-
-      </div>
-
         <div className={`${styles.widget_footer}`}>
           <a className="btn btn-nav block" onClick={() => setSkip(skip+1)}>
             <span className="btn__text">Show {take} more</span>
-          <span className="btn__caret_down"></span>
+            <span className="btn__caret_down"></span>
           </a>
         </div>
         :
         ""
       }
+
     </div>
 
   );
