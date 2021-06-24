a58828305ac2bdc7956f07cb33610d3b700c74c1
Hieu Nguyen <nnth83@gmail.com>
2021-06-24 17:23:38 +0700

    Update Project.js

--- components/cards/Project.js
+++ components/cards/Project.js
@@ -21,12 +21,12 @@ export const ProjectLoader = (props) => (
         // style={{ width: '100%' }}
         {...props}
       >
-        <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
-        <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
-        <rect x="0" y="56" rx="3" ry="3" width="380" height="6" />
-        <rect x="-1" y="71" rx="3" ry="3" width="360" height="6" />
-        <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-        <circle cx="20" cy="20" r="20" />
+        <rect x="0" y="0" rx="3" ry="3" width="40" height="40" />
+        <rect x="48" y="8" rx="3" ry="3" width="128" height="6" />
+        <rect x="48" y="24" rx="3" ry="3" width="80" height="6" />
+        <rect x="48" y="56" rx="3" ry="3" width="380" height="6" />
+        <rect x="48" y="71" rx="3" ry="3" width="360" height="6" />
+        <rect x="48" y="88" rx="3" ry="3" width="178" height="6" />
       </ContentLoader>
     </div>
   </div>
