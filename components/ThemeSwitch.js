aaa6a5f6f60f19c0d73768d4769bd4a39f84c5c6
Khanh Le <khanhle@Khanhs-iMac.local>
2021-07-02 11:58:58 +0700

    simple dark/light switch

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,69 +1,13 @@
 // components/theme-switch.tsx
-import { useState, useEffect } from 'react';
-import { useTheme } from 'next-themes';
-import Switch from 'react-switch';
-import { IconContext } from 'react-icons';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
 
 const ThemeSwitch = () => {
-  const { theme, setTheme } = useTheme();
-
-  const dark = theme === 'dark' ? true : false;
-
-  const [checked, setChecked] = useState(dark);
-  const [mounted, setMounted] = useState(false);
-
-  const handleChange = (nextChecked) => {
-    setChecked(nextChecked);
-  };
-
-  // When mounted on client, now we can show the UI
-  useEffect(() => setMounted(true), []);
-
-  useEffect(() => {
-    setTheme(checked ? 'dark' : 'light');
-  }, [checked, setTheme]);
-
-  if (!mounted) return null;
+  const { theme, setTheme } = useTheme()
 
   return (
-    <Switch
-      onChange={handleChange}
-      checked={checked}
-      aria-label="switch between day and night themes"
-      offColor="#D1D5DB"
-      onColor="#D1D5DB"
-      onHandleColor="#fff"
-      offHandleColor="#fff"
-      handleDiameter={20}
-      uncheckedIcon={
-        <div className="flex justify-center items-center h-full">
-          <IconContext.Provider
-            value={{
-              color: '#374151',
-              size: '0%',
-            }}
-          >
-            <FaSun />
-          </IconContext.Provider>
-        </div>
-      }
-      checkedIcon={
-        <div className="flex justify-center items-center h-full">
-          <IconContext.Provider
-            value={{
-              color: '#374151',
-              size: '60%',
-            }}
-          >
-            <FaMoon />
-          </IconContext.Provider>
-        </div>
-      }
-      height={28}
-      width={56}
-    />
-  );
-};
+      <a className="xzy" onClick={e => setTheme(theme == 'dark' ? 'light' : 'dark')}>xxx</a>
+  )
+}
 
-export default ThemeSwitch;
+export default ThemeSwitch
