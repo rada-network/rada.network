a0ff2d2b2a392d1dc8a8e634b55573f62c15e3bf
Hieu Nguyen <nnth83@gmail.com>
2021-07-09 17:05:19 +0700

    Update Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import styles from '../styles/modules/Navbar.module.css'
-
-
 import {Wallet} from "./Wallet"
 import SearchInput from "./search"
 import ThemeSwitch from "./ThemeSwitch"
