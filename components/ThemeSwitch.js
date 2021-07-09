39133c85aa47d68aef696696fb37caa908dbef03
Khanh Le <khanhle@Khanhs-iMac.local>
2021-07-09 12:06:04 +0700

    render default/system theme

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,25 +1,30 @@
 // components/theme-switch.tsx
 import { useTheme } from 'next-themes'
+import { useEffect } from 'react'
 
 const ThemeSwitch = () => {
-  const { theme, setTheme } = useTheme('light')
+  const { theme, setTheme, resolvedTheme } = useTheme()
 
-  if (theme == "light"){
+  // update system to specific one
+  useEffect(() => {
+    if (['dark', 'light'].indexOf(theme) === -1 && ['dark', 'light'].indexOf(resolvedTheme) !== -1) setTheme(resolvedTheme)
+  }, [resolvedTheme])
 
-  return (
-    <a className="btn nav-btn btn-switch-theme" onClick={e => setTheme(theme == 'dark' ? 'light' : 'dark')} title="Swith to Dark Theme">
+  // not render on SSR
+  if (!theme) return ''
+
+  return theme == "light" ?
+  (
+    <a className="btn nav-btn btn-switch-theme" onClick={e => setTheme('dark')} title="Swith to Dark Theme">
       <span className="icon text-base leading-5"><i className="fad fa-moon" /></span>
     </a>
   ) 
-  }
-
-  else {
-    return (
-      <a className="btn nav-btn btn-switch-theme" onClick={e => setTheme(theme == 'dark' ? 'light' : 'dark')} title="Swith to Light Theme">
-        <span className="text-base leading-5"><i className="fad fa-sun" /></span>
-      </a>
-    ) 
-  }
+  :
+  (
+    <a className="btn nav-btn btn-switch-theme" onClick={e => setTheme('light')} title="Swith to Light Theme">
+      <span className="text-base leading-5"><i className="fad fa-sun" /></span>
+    </a>
+  ) 
 
 }
 
