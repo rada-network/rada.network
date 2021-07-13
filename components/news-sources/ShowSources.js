98c8411c443b05dc03cf2f935c51231b9f842dbf
van010@github.com <itmannavys@gmail.com>
2021-07-14 01:30:14 +0700

    showsources: update news source CoinGape.com

--- components/news-sources/ShowSources.js
+++ components/news-sources/ShowSources.js
@@ -7,10 +7,11 @@ export default function showSources({source}) {
   // const coindeskLogo = "https://seeklogo.com/images/C/coindesk-logo-C8CDEB939F-seeklogo.com.png"
   const cointeleLogo = "images/logo-sources/cointelegraph.png"
   const adapulseLogo = 'images/logo-sources/adapulse.png'
+  const coingapeLogo = 'images/logo-sources/coingape.png'
 
-  const listLogos = [forumCardanoLogo, iohkLogo, coindeskLogo, cointeleLogo, adapulseLogo]
-  const displaySources = ['Cardano Foundation', 'IOHK', 'CoinDesk', 'CoinTelegraph', 'AdaPulse']
-  const listSources = ['forum.cardano', 'iohk', 'coindesk', 'cointele', 'adapulse']
+  const listLogos = [forumCardanoLogo, iohkLogo, coindeskLogo, cointeleLogo, adapulseLogo, coingapeLogo]
+  const displaySources = ['Cardano Foundation', 'IOHK', 'CoinDesk', 'CoinTelegraph', 'AdaPulse', 'CoinGape']
+  const listSources = ['forum.cardano', 'iohk', 'coindesk', 'cointele', 'adapulse', 'coingape']
   for (const [i, value] of listSources.entries()) {
     if (source.toLowerCase().includes(value)) {
       return (
