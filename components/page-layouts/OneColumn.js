ac7ff5b98776ca522fa066bb3ad588a1a3e22e30
tan <tanz.cntt@gmail.com>
2021-07-16 14:25:44 +0700

    edit site metadata

--- components/page-layouts/OneColumn.js
+++ components/page-layouts/OneColumn.js
@@ -5,7 +5,7 @@ import { Footer } from "../Footer";
 export const Layout = ({children, extraClass, meta}) => {
   return (
     <>
-    <Head title={meta?.title} description={meta?.description} />
+    <Head title={meta?.title} description={meta?.description} keyword={meta.keyword} />
     <div className={`wrapper ${extraClass || ''}`}>
       <Navbar />
       
--- pages/_app.js
+++ pages/_app.js
@@ -10,12 +10,31 @@ import { ThemeProvider } from 'next-themes';
 
 
 import { configure } from "mobx"
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+import * as ga from '../lib/ga'
 
 configure({
   enforceActions: "never",
 })
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <Component {...pageProps} />
   )
