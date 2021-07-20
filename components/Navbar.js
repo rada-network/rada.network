98401f9715fb5786917c2fe51f8278c641c1f981
Hieu Nguyen <nnth83@gmail.com>
2021-07-20 18:31:22 +0700

    New UI

--- components/Navbar.js
+++ components/Navbar.js
@@ -81,33 +81,10 @@ export const Navbar = () => {
       </div>
     </nav>
 
-    <ThemeSwitch />
-
-    {/* Mobile Nav
-    <div className="navbar-app">
-
-      <NavItem href="/">
-        <span className="icon"><i className="fad fa-home"></i></span>
-        <span className="nav-item--text">Explore</span>
-      </NavItem>
-      <NavItem href="/explore/news">
-        <span className="icon"><i className="fad fa-newspaper"></i></span>
-        <span className="nav-item--text">News</span>
-      </NavItem>
-      <NavItem href="/explore/social">
-        <span className="icon"><i className="fad fa-fire-alt"></i></span>
-        <span className="nav-item--text">Signals</span>
-      </NavItem>
-      <NavItem href="/explore/projects">
-        <span className="icon"><i className="fad fa-code-branch"></i></span>
-        <span className="nav-item--text">Projects</span>
-      </NavItem>
-      <NavItem href="/explore/blog">
-        <span className="icon"><i className="fad fa-pen-nib"></i></span>
-        <span className="nav-item--text">Blog</span>
-      </NavItem>
-
-    </div> */}
+    <div className="hidden lg:flex">
+      <ThemeSwitch />
+    </div>
+    
     </>
   );
 }
