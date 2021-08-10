67612be14e78afde5bfb8829d1636db0c52d51e2
tan <tanz.cntt@gmail.com>
2021-08-11 01:51:26 +0700

    add language vi,en

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ module.exports = (phase) => {
         i18n: {
             // These are all the locales you want to support in
             // your application
-            locales: ['en', 'vi', 'nl-NL'],
+            locales: ['en', 'vi'],
             // This is the default locale you want to be used when visiting
             // a non-locale prefixed path e.g. `/hello`
             defaultLocale: 'en',
