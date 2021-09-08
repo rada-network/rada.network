f6a8271f28c6c6d6903e965567641c7f9ccd17d2
Hieu Nguyen <nnth83@gmail.com>
2021-09-08 21:37:19 +0700

    - Add "rada" cat to nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,7 @@ export const Navbar = ({dataStore,detailStore}) => {
           {/* Logo */}
           <Link href={`/`}>
             <a className="flex flex-col w-full justify-center items-center">
-              <img className="logo--img" src="/images/dhunt.svg" alt="RADA.CO" />
+              <img className="logo--img" src="/images/rada.svg" alt="RADA NETWORK" />
               <strong className="logo--text mt-1">
                 <span>RADA</span>
               </strong>
@@ -32,18 +32,38 @@ export const Navbar = ({dataStore,detailStore}) => {
 
         {/* Main Nav */}
         <div className={`navbar-main`} >
+
           <NavItem dataStore={dataStore} detailStore={detailStore} href={"/" + dataStore.lang + "/apps/explore/all"} type={"all"}>
             <span className="icon"><i className="fad fa-rss" /></span>
             <span className="nav-item--text">{t("Explore")}</span>
           </NavItem>
+
+          <NavItem dataStore={dataStore} detailStore={detailStore}  href={"/" + dataStore.lang + "/apps/explore/rada"} type={"rada"}>
+            <span className="icon icon-rada">
+              <svg class="rada-svg" viewBox="4 4 32 32" xmlns="http://www.w3.org/2000/svg">
+                <path class="inline-rec" d="M18 11.1547C19.2376 10.4402 20.7624 10.4402 22 11.1547L26.6603 13.8453C27.8979 14.5598 28.6603 15.8803 28.6603 17.3094V22.6906C28.6603 24.1197 27.8979 25.4402 26.6603 26.1547L22 28.8453C20.7624 29.5598 19.2376 29.5598 18 28.8453L13.3397 26.1547C12.1021 25.4402 11.3397 24.1197 11.3397 22.6906V17.3094C11.3397 15.8803 12.1021 14.5598 13.3397 13.8453L18 11.1547Z" fill="#374151"/>
+                <path class="inline-stroke" d="M20 2L20.8806 15.1519C20.9757 16.5717 22.4811 17.4409 23.7582 16.8133L35.5885 11L24.6389 18.3386C23.4568 19.1308 23.4568 20.8692 24.6389 21.6614L35.5885 29L23.7582 23.1867C22.4811 22.5591 20.9757 23.4283 20.8806 24.848L20 38L19.1194 24.8481C19.0243 23.4283 17.5189 22.5591 16.2418 23.1867L4.41154 29L15.3611 21.6614C16.5432 20.8692 16.5432 19.1308 15.3611 18.3386L4.41154 11L16.2418 16.8133C17.5189 17.4409 19.0243 16.5717 19.1194 15.152L20 2Z" fill="#fff"/>
+                <circle class="inline-circle" cx="20" cy="7" r="3" fill="#9CA3AF"/>
+                <circle class="inline-circle" cx="20" cy="33" r="3" fill="#9CA3AF"/>
+                <circle class="inline-circle" cx="31.2583" cy="13.5" r="3" transform="rotate(60 31.2583 13.5)" fill="#9CA3AF"/>
+                <circle class="inline-circle" cx="8.74167" cy="26.5" r="3" transform="rotate(60 8.74167 26.5)" fill="#9CA3AF"/>
+                <circle class="inline-circle" cx="8.74167" cy="13.5" r="3" transform="rotate(-60 8.74167 13.5)" fill="#9CA3AF"/>
+                <circle class="inline-circle" cx="31.2583" cy="26.5" r="3" transform="rotate(-60 31.2583 26.5)" fill="#9CA3AF"/>
+              </svg>
+            </span>
+            <span className="nav-item--text">{t("Rada")}</span>
+          </NavItem>
+
           <NavItem dataStore={dataStore} detailStore={detailStore}  href={"/" + dataStore.lang + "/apps/explore/news"} type={"news"}>
             <span className="icon"><i className="fad fa-newspaper" /></span>
             <span className="nav-item--text">{t("News")}</span>
           </NavItem>
+
           <NavItem dataStore={dataStore} detailStore={detailStore}  href={"/" + dataStore.lang + "/apps/explore/video"}  type={"video"}>
             <span className="icon"><i className="fad fa-icons" /></span>
             <span className="nav-item--text">{t("Video")}</span>
           </NavItem>
+
           <NavItem dataStore={dataStore} detailStore={detailStore}  href={"/" + dataStore.lang + "/apps/explore/social"} type={"social"} className="disabled">
             <span className="icon"><i className="fad fa-fire-alt" /></span>
             <span className="nav-item--text">{t("Signals")}</span>
@@ -51,8 +71,9 @@ export const Navbar = ({dataStore,detailStore}) => {
           </NavItem>
 
           {/* Example of additional filters */}
-          <span className="nav-item--divider"></span>
-          <NavItem dataStore={dataStore} detailStore={detailStore}  href={"/" + dataStore.lang + "/apps/explore/games"}  type={"games"} className="disabled">
+          {/* <span className="nav-item--divider"></span> */}
+
+          {/* <NavItem dataStore={dataStore} detailStore={detailStore}  href={"/" + dataStore.lang + "/apps/explore/games"}  type={"games"} className="disabled">
             <span className="icon"><i className="fad fa-chess-knight" /></span>
             <span className="nav-item--text">{t("Games")}</span>
             <span className="nav-item--badge">{t("Soon")}</span>
@@ -61,7 +82,7 @@ export const Navbar = ({dataStore,detailStore}) => {
             <span className="icon"><i className="fad fa-coins" /></span>
             <span className="nav-item--text">{t("DeFi")}</span>
             <span className="nav-item--badge">{t("Soon")}</span>
-          </NavItem>
+          </NavItem> */}
 
           {/*<NavItem href="/explore/projects">*/}
           {/*  <span className="icon"><i className="fad fa-code-branch" /></span>*/}
