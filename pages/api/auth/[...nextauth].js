c588f43d2189b3fc5b5c9f96527cb94e90c13092
tan <tanz.cntt@gmail.com>
2021-09-14 01:24:24 +0700

    social facebook

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,10 @@ const options = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
+    Providers.Facebook({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      })
   ],
   debug: false
 }
