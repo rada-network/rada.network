08618223e042cb56d3feedd55f38bd7d4a45ec5f
Hieu Nguyen <nnth83@gmail.com>
2021-10-29 05:52:53 +0700

    Add Example Page Layout for Projects List

--- components/card-layouts/concepts/launchpad/ProjectsList.js
+++ components/card-layouts/concepts/launchpad/ProjectsList.js
@@ -18,76 +18,87 @@ export const ProjectsList = ({title, extraClass}) => {
   return (
     <div className="page page-full page-projects-list">
       <div className="w-limiter-lg no-padding scrollbar" ref={scrollBox}>
-        <h2 className="p-4 font-semibold text-center">Public sales</h2>
-        <div className="projects--wrapper">
 
-          <CardProject
-            title="Moniwar" 
-            img="https://picsum.photos/600/300?random=1"
-            tokenLogo="./../../token-logos/moniwar.png"
-            description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. "
-            type="Contributor-only"
-            requirement="Requirement"
-            allocation="1"
-          />
+        <div className="section">
 
-          <CardProject
-            title="Pegaxy"
-            img="https://picsum.photos/600/300?random=2"
-            tokenLogo="./../../token-logos/pegaxy.png"
-            description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
-            type="Contributor-only"
-            requirement="Requirement"
-            allocation="1"
-          />
+          <div className="section-header !justify-center">
+            <h2 className="p-4 font-semibold text-center">Public sales</h2>
+          </div>
 
-          <CardProject
-            title="DareNFT"
-            img="https://picsum.photos/600/300?random=3"
-            tokenLogo="./../../token-logos/darenft.png"
-            description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
-            type="Contributor-only"
-            requirement="Requirement"
-            allocation="1"
-          />
-          
+          <div className="section-body projects--wrapper">
 
+            <CardProject
+              title="Moniwar" 
+              img="https://picsum.photos/600/300?random=1"
+              tokenLogo="./../../token-logos/moniwar.png"
+              description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. "
+              type="Contributor-only"
+              requirement="Requirement"
+              allocation="1"
+            />
+
+            <CardProject
+              title="Pegaxy"
+              img="https://picsum.photos/600/300?random=2"
+              tokenLogo="./../../token-logos/pegaxy.png"
+              description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
+              type="Contributor-only"
+              requirement="Requirement"
+              allocation="1"
+            />
+
+            <CardProject
+              title="DareNFT"
+              img="https://picsum.photos/600/300?random=3"
+              tokenLogo="./../../token-logos/darenft.png"
+              description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
+              type="Contributor-only"
+              requirement="Requirement"
+              allocation="1"
+            />
+            
+          </div>
         </div>
 
-        <h2 className="p-4 font-semibold text-center">Private sales</h2>
-        <div className="projects--wrapper">
-
-          <CardProject
-            title="Thetan Arena" 
-            img="https://picsum.photos/600/300?random=4"
-            tokenLogo="./../../token-logos/thetan.png"
-            description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. "
-            type="Contributor-only"
-            requirement="Requirement"
-            allocation="1"
-          />
-
-          <CardProject
-            title="Elemon"
-            img="https://picsum.photos/600/300?random=5"
-            tokenLogo="./../../token-logos/elemon.png"
-            description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
-            type="Contributor-only"
-            requirement="Requirement"
-            allocation="1"
-          />
-
-          <CardProject
-            title="DareNFT"
-            img="https://picsum.photos/600/300?random=6"
-            tokenLogo="./../../token-logos/darenft.png"
-            description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
-            type="Contributor-only"
-            requirement="Requirement"
-            allocation="1"
-          />
+        <div className="section">
           
+          <div className="section-header !justify-center">
+            <h2 className="p-4 font-semibold">Private sales</h2>
+          </div>
 
+          <div className="section-body projects--wrapper">
+
+            <CardProject
+              title="Thetan Arena" 
+              img="https://picsum.photos/600/300?random=4"
+              tokenLogo="./../../token-logos/thetan.png"
+              description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. "
+              type="Contributor-only"
+              requirement="Requirement"
+              allocation="1"
+            />
+
+            <CardProject
+              title="Elemon"
+              img="https://picsum.photos/600/300?random=5"
+              tokenLogo="./../../token-logos/elemon.png"
+              description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
+              type="Contributor-only"
+              requirement="Requirement"
+              allocation="1"
+            />
+
+            <CardProject
+              title="DareNFT"
+              img="https://picsum.photos/600/300?random=6"
+              tokenLogo="./../../token-logos/darenft.png"
+              description="Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
+              type="Contributor-only"
+              requirement="Requirement"
+              allocation="1"
+            />
+            
+          </div>
         </div>
 
       </div>
