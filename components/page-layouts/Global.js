d9ba81db184628e473aa4a5a1de7306bb9b7a7f3
tan <tanz.cntt@gmail.com>
2021-10-30 10:50:11 +0700

    add layout background

--- components/page-layouts/Global.js
+++ components/page-layouts/Global.js
@@ -7,20 +7,36 @@ import Profile from "../Profile";
 
 import {LanguageSwitch} from "../LanguageSwitch";
 import Screen from "../utils/Responsive";
+import { useRef,useEffect } from "react";
 
 export const Layout =  ({children,meta}) => {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current.setAttribute("style",`background-image: url("https://source.unsplash.com/random/1600x900")`)
+  },[])
+  const handleChangeBg = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    ref.current.setAttribute("style",`background-image: url("https://source.unsplash.com/random/1600x900?time=${(new Date()).getTime()}")`)
+  }
+
+  const handleRemoveBg = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    ref.current.setAttribute("style",`background-image: none`)
+  }
   return (
     <>
     <Head meta={meta} />
 
-    <div className={`body-decor`}>
+    <div ref={ref} className={`body-decor`}>
     </div>
 
     <div className={`body-decor--text`}>
       <p className="mb-1">&copy; Photo from Unsplash</p>
       <div className="flex">
-        <a href="#"><i className="far fa-random"></i> New Photo</a>
-        <a href="#" className="ml-2"><i className="far fa-minus-circle"></i> Remove</a>
+        <a href="#" onClick={handleChangeBg}><i className="far fa-random"></i> New Photo</a>
+        <a href="#" onClick={handleRemoveBg} className="ml-2"><i className="far fa-minus-circle"></i> Remove</a>
       </div>
     </div>
 
