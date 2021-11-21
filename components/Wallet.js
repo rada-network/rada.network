33b9e6b8496fee86782839f9a25afa723283eb63
tan <tanz.cntt@gmail.com>
2021-11-21 21:49:29 +0700

    add wallet address in topbar

--- components/Wallet.js
+++ components/Wallet.js
@@ -298,15 +298,40 @@ export const WalletProfile = ({type}) => {
         {t("connect")}</button>
       </>
     ) : (
-      <button
-        className="btn btn-primary my-2 px-2 ml-auto lg:mr-12 text-sm flex"
-        onClick={() => handleDisconnectWallet()}
-      >
-        <span class="icon mr-2 opacity-60">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Wallet</title><path fill="currentColor" d="M18.874,18a3.757,3.757,0,0,1-3.669-4.538A3.819,3.819,0,0,1,18.982,10.5h2.393a.5.5,0,0,0,.5-.5c0-.793,0-2.254,0-2.254a2.5,2.5,0,0,0-2.5-2.5c-4.476,0,8.862-.01-14-.01a.75.75,0,0,1,0-1.5h13.25a.25.25,0,0,0,.25-.249V3.25c0-1.379-1.57-2.5-3.5-2.5h-11a3.5,3.5,0,0,0-3.5,3.5v15.5a3.5,3.5,0,0,0,3.5,3.5h15a2.5,2.5,0,0,0,2.5-2.5v-2.5a.25.25,0,0,0-.25-.25Z"/><path fill="currentColor"  d="M21.874,12h-3a2.25,2.25,0,1,0,0,4.5h3a1.506,1.506,0,0,0,1.25-1.5V13.5A1.506,1.506,0,0,0,21.874,12Z"/></svg>
-        </span>
-        {t("disconnect")}
-      </button>
+      <div className="my-2 px-2 ml-auto lg:mr-12 text-sm flex items-center">
+          {_.isEmpty(account) ? (
+            <span>
+              {t("no connection", { provider: "wallet" })}
+            </span>
+          ) : (
+            <>
+              <div>
+                <strong>{`${account.substr(
+                  0,
+                  6
+                )}...${account.substr(
+                  -4
+                )} `}</strong>
+              </div>
+              {store.network == "bsc" ?
+              <span className="badge badge-coin relative ml-2 mr-2">
+                BSC
+              </span>
+              :
+              <span className="badge badge-coin relative ml-2 mr-2">
+                ETHEREUM
+              </span>
+              }
+              <button
+                className="btn btn-default text-right"
+                onClick={() => handleDisconnectWallet()}
+              >
+                {t("disconnect")}
+              </button>
+            </>
+          )}
+
+        </div>
     )}
     </>
     }
