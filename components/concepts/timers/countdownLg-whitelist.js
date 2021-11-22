8c351e207309f3aaa8c043622450101db116e8e0
Hieu Nguyen <nnth83@gmail.com>
2021-11-22 19:40:40 +0700

    #concept Launchpad - ComingSoon

--- components/concepts/timers/countdownLg-whitelist.js
+++ components/concepts/timers/countdownLg-whitelist.js
@@ -30,34 +30,6 @@ const CountDownLg = ({}) => {
 
         </div>
 
-        <div className="flex flex-col md:flex-row mt-8 max-w-xl justify-evenly mx-auto">
-          <div className="w-full md:w-1/2 p-4 my-2 md:m-2 bg-primary-700 dark:bg-primary-700 text-white rounded-lg flex items-center cursor-pointer">
-            <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-gray-900 bg-opacity-50 rounded-full flex-shrink-0 mr-4 shadow">
-              <i class="fa-duotone fa-hand-holding-heart"></i>
-            </span>
-            <div>
-              <p className="mb-1 opacity-80">Get more RIR with Share2Earn</p>
-              <a href="/launchverse/parallel/share2earn" class="group text-white">
-                <span class="text-sm">Joint now</span>
-                <span class="icon text-xs relative left-1 group-hover:left-2 transition-all"><i class="fas fa-angle-right"></i></span>
-              </a>
-            </div>
-          </div>
-
-          <div className="w-full md:w-1/2 p-4 my-2 md:m-2 bg-gray-100 dark:bg-gray-700 rounded-lg flex cursor-pointer">
-            <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-900 rounded-full flex-shrink-0 mr-4 shadow">
-              <i class="fad fa-info"></i>
-            </span>
-            <div>
-              <p className="mb-1 opacity-80">Joint the launchverse is really simple</p>
-              <a href="/launchverse/parallel/share2earn" class="group text-white">
-                <span class="text-sm">Learn more</span>
-                <span class="icon text-xs relative left-1 group-hover:left-2 transition-all"><i class="fas fa-angle-right"></i></span>
-              </a>
-            </div>
-          </div>
-        </div>
-
       </div>
 
     </>
