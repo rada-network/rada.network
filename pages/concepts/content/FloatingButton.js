e755c0af5807834cc1c6ea79deec53d76356a942
Hieu Nguyen <nnth83@gmail.com>
2021-12-03 03:31:42 +0700

    Update FloatingButton.js

--- pages/concepts/content/FloatingButton.js
+++ pages/concepts/content/FloatingButton.js
@@ -24,14 +24,13 @@ export default function FAB() {
               enter="transition ease-out duration-100"
               enterFrom="transform origin-bottom-right opacity-100 scale-0"
               enterTo="transform origin-bottom-right opacity-100 scale-100"
-              leave="transition origin-top-left ease-in duration-75"
+              leave="transition ease-in duration-75"
               leaveFrom="transform origin-top-left opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-0"
+              leaveTo="transform origin-bottom-right opacity-0 scale-0"
             >
               <Popover.Panel className="absolute z-10 transform right-full -mr-8 bottom-9 w-80">
                 <div className="overflow-hidden rounded-lg shadow-lg">
                   <div className="popper poper-toc p-4 !mt-0">
-                  <div class="page-close"><button class="btn popper-close-btn"><span class="icon"><i class="fa-solid fa-times"></i></span></button></div>
                     <Toc />
                   </div>
                 </div>
