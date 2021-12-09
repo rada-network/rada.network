520d23ea0d15af00e0c49dc0bc90653839d6045b
Hieu Nguyen <nnth83@gmail.com>
2021-12-10 03:45:48 +0700

    #launchverse New Home

--- pages/concepts/launchpad/projects-Min.js
+++ pages/concepts/launchpad/projects-Min.js
@@ -1,37 +1,27 @@
 import { Head } from "../../../components/Head";
 import { Topbar } from "../../../components/Topbar";
 import { Navbar } from "../../../components/Navbar";
-import TokenInfo from "../../../components/token/TokenInfo";
 import { ProjectsList } from "../../../components/card-layouts/concepts/launchpad/ProjectsList";
 import {PostsListWrapper} from "../../../components/card-layouts/PostsList";
-import { BscSvg, UsdtSvg, LaunchSvg } from "../../../components/svg/SvgIcons";
-
-// Cards Concepts
-import TokenMeta from "../_components/cards/launchpad/TokenMeta";
-import MainActions from "../_components/cards/launchpad/MainActions-whitelist-countdown";
 
 import ThemeSwitch from "../../../components/ThemeSwitch"
 import Profile from "../../../components/Profile";
 
-import { useState, useEffect, createRef } from 'react'
 
 import {LanguageSwitch} from "../../../components/LanguageSwitch";
 import Screen from "../../../components/utils/Responsive";
 import { observer } from "mobx-react";
 import { usePageStore } from "../../../lib/usePageStore";
-import TokenContent from "./TokenContent";
-import TokenBrief from "./TokenBrief";
-import TokenNav from "./TokenNav";
-import BackgroundWrapper from "../../../components/card-layouts/concepts/launchpad/BackgroundWrapper";
 
-let step = 1;
+import { useRef,useEffect } from "react";
+import BackgroundWrapper from "../../../components/card-layouts/concepts/launchpad/BackgroundWrapper";
 const Layout = observer((props) => {
 
   const {dataStore,detailStore,voteStore} = usePageStore()
   dataStore.type = "projects"
   dataStore.lang = props.lang
   const meta = {}
-  let step = 1;
+
   return (
     <>
       <Head meta={meta} />
@@ -63,37 +53,29 @@ const Layout = observer((props) => {
           </Screen>
 
           <div className={`pane-center`}>
-            <div className="pane-center--main">
+            <div className="pane-center--main glassmorphism">
 
               <div className="pane-content">
                 <div className="pane-content--sec pane-content-active !w-full">
-
-                  <TokenNav />
-
-                  <div className="pane-content--sec--main grid scrollbar">
-
-                    <div className="page page-full page-project-details !pt-0">
-                      <div className="w-limiter-lg">
-
-                        <div>
-                          
-                          <TokenBrief />
-                       
-
-                          <div className="section-body p-4 md:p-4">
-
-                            <TokenContent />
-
-                            {/* Main Action Card */}
-                            <div className="grid grid-cols-1 mt-4">
-                              <MainActions />
-                            </div>
-                            {/* END: Main Action Card */}
-
-                          </div>          
-                        </div>
+                <div className="pane-content--sec--top !block">
+                  <div className="flex h-full relative lg:px-3">
+                    
+                    <div className="tabbar page-tabs">
+                      <div className="tabbar--main">
+                        <a href="#" className="tab-item tab-item--active">Public</a>
+                        <span className="tab-item--divider"></span>
+                        <a href="#" className="tab-item">Private</a>
                       </div>
                     </div>
+                  </div>
+                </div>
+
+                  <div className="pane-content--sec--main grid scrollbar dark:!bg-gray-900 !bg-opacity-70">
+
+                    <div className="page page-full page-project-details !pt-0">
+                      <ProjectsList /> 
+                    </div>
+
                   </div>
 
                 </div>
