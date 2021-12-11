fc47877ffb7d7419bea72515e1557028e76ef0d1
Hieu Nguyen <nnth83@gmail.com>
2021-12-11 11:50:22 +0700

    #concept LaunchVerse V2

--- pages/concepts/_components/cards-layout/ProjectsList-v2.js
+++ pages/concepts/_components/cards-layout/ProjectsList-v2.js
@@ -14,14 +14,14 @@ export default function ProjectsList({title, extraClass}){
 
       <div className="projects-list">
         <CardProject
-          title="The Paralell - Open IDO" 
+          title="The Paralell - Private" 
           img="./../../placeholders/parallel-cover.jpg"
           tokenLogo="./../../token-logos/theparallel.png"
           raise="45,000 USDT"
           progressToken="100,000"
           target="100,000"
           progressPercentage="2%"
-          type="public"
+          type="private"
           token="PRL"
           countdown="2 hours"
           status="open"
@@ -36,7 +36,7 @@ export default function ProjectsList({title, extraClass}){
           progressToken="100,000"
           target="100,000"
           progressPercentage="2%"
-          type="public"
+          type="community"
           token="PRL"
           countdown="2 hours"
           status="open"
