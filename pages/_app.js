b3d01e6ab1c2ee3ed298f06c5a0121d828ce99f8
Lu Nguyen <lu@redweb.dk>
2021-12-13 15:17:25 +0700

    Fix og

--- pages/_app.js
+++ pages/_app.js
@@ -160,11 +160,13 @@ const TokenRankingStore = ({
 }) => {
   // const store = useStore(pageProps.initialState)
 
-  // Fix  Avoid Hydration Mismatch #https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => setMounted(true), []);
+  if (process.env.NODE_ENV !== "production") {
+    // Fix  Avoid Hydration Mismatch #https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => setMounted(true), []);
 
-  if (!mounted) return null;
+    if (!mounted) return null;
+  }
 
   return (
     <ThemeProvider attribute="class">
