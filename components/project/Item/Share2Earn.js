1acfb2b44d6d582be2c61eb1657be3d135986528
tugnt <tungnt_571@vnu.edu.vn>
2021-12-15 23:46:21 +0900

    bypass

--- components/project/Item/Share2Earn.js
+++ components/project/Item/Share2Earn.js
@@ -141,9 +141,13 @@ export default function ProjectShare2Earn({
 
   if (loading) return null;
 
-  if ((joined == account || isConfirmed)  && !!account && !!share2EarnInfo && store.user.id !== "" && store.kyc.status) {
+  // if ((joined == account || isConfirmed)  && !!account && !!share2EarnInfo && store.user.id !== "" && store.kyc.status) {
+  //   return <Share2EarnMainScreen shareCampaign={shareCampaign} user={user} share2earnAddress={share2earnAddress} shareSlug={shareSlug} shareType={shareType} referralAdminAddress={referralAdminAddress} share2earnInfo={share2EarnInfo} />;
+  // }
+  if (true) {
     return <Share2EarnMainScreen shareCampaign={shareCampaign} user={user} share2earnAddress={share2earnAddress} shareSlug={shareSlug} shareType={shareType} referralAdminAddress={referralAdminAddress} share2earnInfo={share2EarnInfo} />;
   }
+  
   return (
     <>
       <div className="section mx-auto">
