8ea83a26b45c683526865aa8b6add6a19291fa93
Hieu Nguyen <nnth83@gmail.com>
2021-12-16 00:01:47 +0700

    -Finish Responsive

--- components/project/List/ProjectsList.js
+++ components/project/List/ProjectsList.js
@@ -20,75 +20,80 @@ export default function ProjectsList({ title, extraClass, projects, isComing}) {
                 >
                   {({ open }) => (
                     <>
-                      <Disclosure.Button as="div" className="pools-container--toggle">
+                      <Disclosure.Button as="div" className="pools-container--toggle" title={project.content.title}>
+
                         <div className="project-logo">
                           <img src={project.thumbnail_uri} alt="The Parallels" />
                         </div>
-                        <div className="flex w-1/4 flex-shrink-0 justify-between items-baseline">
-                          <h4>
-                            {project.content.title}
-                          </h4>
-                          <span className="badge badge-coin badge-coin-lg mr-4">${project.token.symbol}</span>
+
+                        <div className="flex flex-col md:flex-row w-3/6 md:pr-0 md:w-4/6 lg:w-3/6 flex-shrink-0 ">
+                          <div className="flex md:justify-between items-center md:w-4/6 lg:w-4/6">
+                            <h4 className="line-clamp-1">
+                              {project.content.title}
+                            </h4>
+                            <span className="badge badge-coin md:mr-4">${project.token.symbol}</span>
+                          </div>
+
+                          <div className="project-links">
+                            <ul>
+                              {project.website && (
+                                <li>
+                                  <a class="btn-website" target="_blank" href={project.website} rel="noreferrer">
+                                    <i class="fa-solid fa-globe"></i>
+                                    <span className="sr-only">Website</span>
+                                  </a>
+                                </li>
+                              )}
+
+                              {project.facebook && (
+                                <li>
+                                  <a class="btn-twitter" target="_blank" href={project.facebook} rel="noreferrer">
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <span className="sr-only">Facebook</span>
+                                  </a>
+                                </li>
+                              )}
+
+                              {project.twitter && (
+                                <li>
+                                  <a class="btn-twitter" target="_blank" href={project.twitter} rel="noreferrer">
+                                    <i class="fa-brands fa-twitter"></i>
+                                    <span className="sr-only">Twitter</span>
+                                  </a>
+                                </li>
+                              )}
+
+                              {project.discord && (
+                                <li>
+                                  <a class="btn-discord" target="_blank" href={project.discord} rel="noreferrer">
+                                    <i class="fa-brands fa-discord"></i>
+                                    <span className="sr-only">Discord</span>
+                                  </a>
+                                </li>
+                              )}
+
+                              {project.medium && (
+                                <li>
+                                  <a class="btn-medium" target="_blank" href="#">
+                                    <i class="fa-brands fa-medium"></i>
+                                    <span className="sr-only">Medium</span>
+                                  </a>
+                                </li>
+                              )}
+
+                              {project.telegram && (
+                                <li>
+                                  <a class="btn-medium" target="_blank" href={project.telegram}>
+                                    <i class="fa-brands fa-telegram"></i>
+                                    <span className="sr-only">Telegram</span>
+                                  </a>
+                                </li>
+                              )}
+
+                            </ul>
+                          </div>
                         </div>
 
-                        <div className="project-links">
-                          <ul>
-                            {project.website && (
-                              <li>
-                                <a class="btn-website" target="_blank" href={project.website} rel="noreferrer">
-                                  <i class="fa-solid fa-globe"></i>
-                                  <span className="sr-only">Website</span>
-                                </a>
-                              </li>
-                            )}
-
-                            {project.facebook && (
-                              <li>
-                                <a class="btn-twitter" target="_blank" href={project.facebook} rel="noreferrer">
-                                  <i class="fa-brands fa-facebook"></i>
-                                  <span className="sr-only">Facebook</span>
-                                </a>
-                              </li>
-                            )}
-
-                            {project.twitter && (
-                              <li>
-                                <a class="btn-twitter" target="_blank" href={project.twitter} rel="noreferrer">
-                                  <i class="fa-brands fa-twitter"></i>
-                                  <span className="sr-only">Twitter</span>
-                                </a>
-                              </li>
-                            )}
-
-                            {project.discord && (
-                              <li>
-                                <a class="btn-discord" target="_blank" href={project.discord} rel="noreferrer">
-                                  <i class="fa-brands fa-discord"></i>
-                                  <span className="sr-only">Discord</span>
-                                </a>
-                              </li>
-                            )}
-
-                            {project.medium && (
-                              <li>
-                                <a class="btn-medium" target="_blank" href="#">
-                                  <i class="fa-brands fa-medium"></i>
-                                  <span className="sr-only">Medium</span>
-                                </a>
-                              </li>
-                            )}
-
-                            {project.telegram && (
-                              <li>
-                                <a class="btn-medium" target="_blank" href={project.telegram}>
-                                  <i class="fa-brands fa-telegram"></i>
-                                  <span className="sr-only">Telegram</span>
-                                </a>
-                              </li>
-                            )}
-
-                          </ul>
-                        </div>
                         <span
                           className={`${open ? 'open' : ''
                             } pools-container--toggle-arrow`}
@@ -105,7 +110,7 @@ export default function ProjectsList({ title, extraClass, projects, isComing}) {
                         leaveTo="transform origin-top scale-y-0 opacity-0"
                       >
                         <Disclosure.Panel className="pools-container--panel">
-                          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
                             {project.project_pool.map((pool) => (
                               <CardProject
                                 isComing={isComing}
