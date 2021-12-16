19ef3d24d15a8ef1cad0c91c89947f8221c8748c
Lu Nguyen <lu@redweb.dk>
2021-12-16 14:55:51 +0700

    Fix theme-image

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,13 @@
 import ImageNext from "next/image";
+import { useState, useEffect } from "react";
 
 const Image = (props) => {
+  // Fix  Avoid Hydration Mismatch #https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   const data = {
     layout: "fixed",
     ...props,
