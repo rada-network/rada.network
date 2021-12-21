4877267f1931a51637944bdd975fac28f073fa16
Lu Nguyen <lu@redweb.dk>
2021-12-21 10:00:41 +0700

    Image

--- components/project/List/CardProjectSmall.js
+++ components/project/List/CardProjectSmall.js
@@ -1,83 +1,92 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
-import Link from "next/link"
+import Link from "next/link";
+import Image from "@components/Image";
 
-export const CardProject = ({project, pool, title, link, img, status, raise, participian, tokenLogo, tokenPrice, countdown, token, progressToken, target, endedin, type, network}) => {
-  const {t,i18n} = useTranslation("launchpad");
-  const [endDate, setEndDate] = useState("")
+export const CardProject = ({
+  project,
+  pool,
+  title,
+  link,
+  img,
+  status,
+  raise,
+  participian,
+  tokenLogo,
+  tokenPrice,
+  countdown,
+  token,
+  progressToken,
+  target,
+  endedin,
+  type,
+  network,
+}) => {
+  const { t, i18n } = useTranslation("launchpad");
+  const [endDate, setEndDate] = useState("");
   useEffect(() => {
-    const options = {year: 'numeric', month: 'short', day: 'numeric' }
-    const endPool = Date.parse(pool.end_date)
-    setEndDate(new Date(endPool).toLocaleDateString("en-US", options))
-  }, [])
-  if (pool.is_hidden) return null
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    const endPool = Date.parse(pool.end_date);
+    setEndDate(new Date(endPool).toLocaleDateString("en-US", options));
+  }, []);
+  if (pool.is_hidden) return null;
   return (
     <Link href={`/${i18n.language}/launchverse/${project.slug}/${pool.slug}`}>
       <div className={`card-project card-project-sm`}>
         <div className="project-content">
-
           <div className="project-content--meta">
             <div className="project-title">
               <div className="project-title--token-logo">
-                <img src={tokenLogo} className="rounded-full" />
+                <Image
+                  src={tokenLogo}
+                  className="rounded-full"
+                  width={32}
+                  height={32}
+                />
               </div>
               <div className="flex items-baseline">
-                <div className="project-title--token-name">
-                  {title}
-                </div>
+                <div className="project-title--token-name">{title}</div>
               </div>
             </div>
 
             <ul className="project-fields">
               <li className="list-pair">
-                <span className="list-key">
-                  {t("Raise")}
-                </span>
+                <span className="list-key">{t("Raise")}</span>
                 <span className="list-value font-semibold">
                   {pool.raise.toLocaleString() + " BUSD"}
                 </span>
               </li>
               <li className="list-pair">
-                <span className="list-key">
-                  {t("Participians")}
-                </span>
+                <span className="list-key">{t("Participians")}</span>
                 <span className="list-value">
                   <span className="font-semibold">{participian}</span>
                 </span>
               </li>
               <li className="list-pair">
-                <span className="list-key">
-                  {t("Progress")}
-                </span>
+                <span className="list-key">{t("Progress")}</span>
                 <span className="list-value">
                   <span className="font-semibold">{progressToken}</span>
                   <span className="opacity-70">/{target}</span> {token}
                 </span>
               </li>
               <li className="list-pair">
-                <span className="list-key">
-                  {t("Ended in")}
-                </span>
+                <span className="list-key">{t("Ended in")}</span>
                 <span className="list-value">
                   <span className="font-semibold">{endDate}</span>
                 </span>
               </li>
             </ul>
-
           </div>
 
           <div className="project-status">
-            <span className={`label label-${status}`}>
-              {status}
-            </span>
+            <span className={`label label-${status}`}>{status}</span>
           </div>
         </div>
         {/* End of project--content */}
       </div>
     </Link>
-    
-  )
-}
+  );
+};
 
-export default CardProject
+export default CardProject;
--- components/project/List/ProjectsList.js
+++ components/project/List/ProjectsList.js
@@ -1,66 +1,81 @@
-import { Fragment } from 'react';
-import { Disclosure, Transition } from '@headlessui/react';
-import ProjectPool from './ProjectPool';
+import { Fragment } from "react";
+import { Disclosure, Transition } from "@headlessui/react";
+import ProjectPool from "./ProjectPool";
+import Image from "@components/Image";
 
-export default function ProjectsList({ title, extraClass, projects}) {
-
-  function checkURL (project) {
+export default function ProjectsList({ title, extraClass, projects }) {
+  function checkURL(project) {
     let socialURL = new Array();
     if (project.website) {
-      socialURL.push(project.website)
+      socialURL.push(project.website);
     }
 
     if (project.twitter) {
-      socialURL.push(project.twitter)
+      socialURL.push(project.twitter);
     }
 
     if (project.telegram) {
-      socialURL.push(project.telegram)
+      socialURL.push(project.telegram);
     }
 
     if (project.medium) {
-      socialURL.push(project.medium)
+      socialURL.push(project.medium);
     }
 
     if (project.discord) {
-      socialURL.push(project.discord)
+      socialURL.push(project.discord);
     }
 
     if (project.facebook) {
-      socialURL.push(project.facebook)
+      socialURL.push(project.facebook);
     }
-    return socialURL
+    return socialURL;
   }
 
   function checkDiscord(project) {
-    return project.website == null || project.twitter == null || project.telegram == null || project.medium == null
+    return (
+      project.website == null ||
+      project.twitter == null ||
+      project.telegram == null ||
+      project.medium == null
+    );
   }
 
   function checkFacebook(project) {
-    return checkDiscord(project) && checkURL(project).length <= 4
+    return checkDiscord(project) && checkURL(project).length <= 4;
   }
 
   return (
     <>
       <div className="projects-section">
-
         <div className="projects-section--subheader">
           <h3 className="">{title}</h3>
         </div>
 
         <div className="w-full">
-          {projects.map(project => {
+          {projects.map((project) => {
             return (
               <div key={project.slug}>
-                <Disclosure  as="div" defaultOpen={project.is_default_open} className="pools-container"
-                  style={{ backgroundImage: `url(${project.cover_uri})`, }}
+                <Disclosure
+                  as="div"
+                  defaultOpen={project.is_default_open}
+                  className="pools-container"
+                  style={{ backgroundImage: `url(${project.cover_uri})` }}
                 >
                   {({ open }) => (
                     <>
-                      <Disclosure.Button as="div" className="pools-container--toggle" title={project.content.title}>
-
+                      <Disclosure.Button
+                        as="div"
+                        className="pools-container--toggle"
+                        title={project.content.title}
+                      >
                         <div className="project-logo">
-                          <img src={project.thumbnail_uri} alt="The Parallels" />
+                          <Image
+                            src={project.thumbnail_uri}
+                            alt={project.content.title}
+                            width={80}
+                            height={80}
+                          />
                         </div>
 
                         <div className="flex flex-col md:flex-row md:pr-0 flex-shrink-0">
@@ -68,15 +83,21 @@ export default function ProjectsList({ title, extraClass, projects}) {
                             <h4 className="line-clamp-1">
                               {project.content.title}
                             </h4>
-                            <span className="badge badge-coin md:mr-4">${project.token.symbol}</span>
+                            <span className="badge badge-coin md:mr-4">
+                              ${project.token.symbol}
+                            </span>
                           </div>
 
                           <div className="project-links">
                             <ul>
-
                               {project.website && (
                                 <li key={`website`}>
-                                  <a className="btn-website" target="_blank" href={project.website} rel="noreferrer">
+                                  <a
+                                    className="btn-website"
+                                    target="_blank"
+                                    href={project.website}
+                                    rel="noreferrer"
+                                  >
                                     <i className="fa-solid fa-globe"></i>
                                     <span className="sr-only">Website</span>
                                   </a>
@@ -85,7 +106,12 @@ export default function ProjectsList({ title, extraClass, projects}) {
 
                               {project.twitter && (
                                 <li key={`twitter`}>
-                                  <a className="btn-twitter" target="_blank" href={project.twitter} rel="noreferrer">
+                                  <a
+                                    className="btn-twitter"
+                                    target="_blank"
+                                    href={project.twitter}
+                                    rel="noreferrer"
+                                  >
                                     <i className="fa-brands fa-twitter"></i>
                                     <span className="sr-only">Twitter</span>
                                   </a>
@@ -94,7 +120,11 @@ export default function ProjectsList({ title, extraClass, projects}) {
 
                               {project.telegram && (
                                 <li key={`telegram`}>
-                                  <a className="btn-medium" target="_blank" href={project.telegram}>
+                                  <a
+                                    className="btn-medium"
+                                    target="_blank"
+                                    href={project.telegram}
+                                  >
                                     <i className="fa-brands fa-telegram"></i>
                                     <span className="sr-only">Telegram</span>
                                   </a>
@@ -103,7 +133,11 @@ export default function ProjectsList({ title, extraClass, projects}) {
 
                               {project.medium && (
                                 <li key={`medium`}>
-                                  <a className="btn-medium" target="_blank" href={project.medium}>
+                                  <a
+                                    className="btn-medium"
+                                    target="_blank"
+                                    href={project.medium}
+                                  >
                                     <i className="fa-brands fa-medium"></i>
                                     <span className="sr-only">Medium</span>
                                   </a>
@@ -112,7 +146,12 @@ export default function ProjectsList({ title, extraClass, projects}) {
 
                               {project.discord && checkDiscord(project) && (
                                 <li key={`discord`}>
-                                  <a className="btn-discord" target="_blank" href={project.discord} rel="noreferrer">
+                                  <a
+                                    className="btn-discord"
+                                    target="_blank"
+                                    href={project.discord}
+                                    rel="noreferrer"
+                                  >
                                     <i className="fa-brands fa-discord"></i>
                                     <span className="sr-only">Discord</span>
                                   </a>
@@ -121,20 +160,25 @@ export default function ProjectsList({ title, extraClass, projects}) {
 
                               {project.facebook && checkFacebook(project) && (
                                 <li key={`facebook`}>
-                                  <a className="btn-twitter" target="_blank" href={project.facebook} rel="noreferrer">
+                                  <a
+                                    className="btn-twitter"
+                                    target="_blank"
+                                    href={project.facebook}
+                                    rel="noreferrer"
+                                  >
                                     <i className="fa-brands fa-facebook"></i>
                                     <span className="sr-only">Facebook</span>
                                   </a>
                                 </li>
                               )}
-
                             </ul>
                           </div>
                         </div>
 
                         <span
-                          className={`${open ? 'open' : ''
-                            } pools-container--toggle-arrow`}
+                          className={`${
+                            open ? "open" : ""
+                          } pools-container--toggle-arrow`}
                         >
                           <i className="fas fa-angle-down"></i>
                         </span>
@@ -147,21 +191,18 @@ export default function ProjectsList({ title, extraClass, projects}) {
                         leaveFrom="transform origin-top opacity-100"
                         leaveTo="transform origin-top opacity-0"
                       > */}
-                        <Disclosure.Panel className="pools-container--panel">
-                          <ProjectPool project={project} ></ProjectPool>
-                        </Disclosure.Panel>
+                      <Disclosure.Panel className="pools-container--panel">
+                        <ProjectPool project={project}></ProjectPool>
+                      </Disclosure.Panel>
                       {/* </Transition> */}
                     </>
                   )}
                 </Disclosure>
               </div>
-            )
+            );
           })}
-
         </div>
-
-
       </div>
     </>
-  )
+  );
 }
