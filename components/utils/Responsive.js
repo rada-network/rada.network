ffddc0df1fc7a4d9491bd27c39d921e1be8cbcd1
Lu Nguyen <lu@redweb.dk>
2021-12-29 11:44:57 +0700

    Fix screen

--- components/utils/Responsive.js
+++ components/utils/Responsive.js
@@ -1,45 +1,49 @@
 import { useEffect, useState, useRef } from "react";
 
-const SCREENS = ['xs', 'sm', 'md', 'lg', 'xl', '2xl']
-const SCREENS_WIDTH = [0, 640, 768, 1023, 1280, 1536]
+const SCREENS = ["xs", "sm", "md", "lg", "xl", "2xl"];
+const SCREENS_WIDTH = [0, 640, 768, 1023, 1280, 1536];
 
-{/* <Screen from="md">xxxx</Screen>
-<Screen upto="md">xxxx</Screen> */}
-
-export const getScreenName = (w) => {
-    let i=SCREENS.length-1
-    while (i>0 && w < SCREENS_WIDTH[i]) i--
-    return SCREENS[i]
+{
+  /* <Screen from="md">xxxx</Screen>
+<Screen upto="md">xxxx</Screen> */
 }
 
-export default function Screen({from, upto, children}) {
-    const [screen, setScreen] = useState(0)
+export const getScreenName = (w) => {
+  let i = SCREENS.length - 1;
+  while (i > 0 && w < SCREENS_WIDTH[i]) i--;
+  return SCREENS[i];
+};
 
-    // setup monitor ww
-    useEffect(() => {
-        const onResize = () => {
-            // setWw(window.innerWidth)
-            const w = window.innerWidth
-            let i = 0
-            while (i<SCREENS.length-1 && w > SCREENS_WIDTH[i+1]) i++
-            setScreen(i)
-        }
-        onResize()
-        window.addEventListener("resize", onResize)
-        return () => {
-            window.removeEventListener("resize", onResize);
-          }
-    }, [])
+export default function Screen({ from, upto, children }) {
+  const [screen, setScreen] = useState();
 
-    let isValid = false
+  // setup monitor ww
+  useEffect(() => {
+    const onResize = () => {
+      // setWw(window.innerWidth)
+      const w = window.innerWidth;
+      let i = 0;
+      while (i < SCREENS.length - 1 && w > SCREENS_WIDTH[i + 1]) i++;
+      setScreen(i);
+    };
+    onResize();
+    window.addEventListener("resize", onResize)
+    return () => {
+        window.removeEventListener("resize", onResize);
+      }
+  }, []);
+
+  let isValid = false;
+  if (screen !== null) {
     if (from && SCREENS.indexOf(from) <= screen) {
-        // from this up
-        isValid = true
+      // from this up
+      isValid = true;
     }
     if (upto && SCREENS.indexOf(upto) >= screen) {
-        // from this down
-        isValid = true
+      // from this down
+      isValid = true;
     }
+  }
 
-    return isValid ? children : ''
+  return isValid ? children : "";
 }
