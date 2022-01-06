bb1d680a933a0c788649136b1b792476df49b56d
Lu Nguyen <lu@redweb.dk>
2022-01-06 14:38:13 +0700

    Tags

--- components/ContentDescription.js
+++ components/ContentDescription.js
@@ -1,6 +1,19 @@
+import WhiteListTag from "@data/whitelisttags";
+import { useRouter } from "next/router";
+
 const ContentDescription = ({ content }) => {
+  const { locale } = useRouter();
+
   if (content) {
     content = content.replace(/<img (.*)>/gi, '<img loading="lazy" $1 />');
+    content = content.replace(/style/gi, "");
+
+    WhiteListTag.forEach((tag) => {
+      content = content.replace(
+        new RegExp(`#${tag}`, "gi"),
+        `<a href="/${locale}/tags/${tag.toLowerCase()}">#${tag}</a>`
+      );
+    });
   }
 
   return <div dangerouslySetInnerHTML={{ __html: content }} />;
---
+++ data/whitelisttags.js
@@ -0,0 +1,3 @@
+const WhiteListTag = ["metaverse", "free2play", "free2earn", "play2earn", "gamefi"];
+
+export default WhiteListTag;
