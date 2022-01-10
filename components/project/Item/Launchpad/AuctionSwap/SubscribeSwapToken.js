5f0ef568469b4cf14146d1274c2299df43a756f9
nttug <thaodantokyo@gmail.com>
2022-01-10 17:47:52 +0900

    add countdown

--- components/project/Item/Launchpad/AuctionSwap/SubscribeSwapToken.js
+++ components/project/Item/Launchpad/AuctionSwap/SubscribeSwapToken.js
@@ -4,22 +4,19 @@ import { useEffect, useState } from "react";
 import useActiveWeb3React from "@utils/hooks/useActiveWeb3React";
 import { ethers, utils } from "ethers";
 import { useTranslation } from "next-i18next";
-
-import { useAuctionSwapInfo} from "@utils/hooks/index";
+import { useAuctionSwapInfo } from "@utils/hooks/index";
 import SwapTokensV2 from "./SwapTokenV2";
 import { useCallWithGasPrice } from "@utils/hooks/useCallWithGasPrice"
 import { toast } from "react-toastify"
 import SocialPromote from "../SocialPromote";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import useChainConfig from "utils/web3/useChainConfig"
 import MiniCountdown from "@components/project/List/Countdown";
-import useStore from "@lib/useStore"
-import OpenDate from "@components/project/Item/Launchpad/OpenDate"
-import { CheckSvg } from "@components/svg/SvgIcons"
-import { poll } from "@ethersproject/web";
+import useStore from "@lib/useStore";
+import OpenDate from "@components/project/Item/Launchpad/OpenDate";
+import ProjectCountdown from "./Countdown";
 
 
-const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
+const SubscribeSwapToken = ({ project, openTime, endTime, currentTime, pool }) => {
   const { t, i18n } = useTranslation("launchpad")
   const rirContract = useRIRContract()
   const bUSDContract = useBUSDContractV2()
@@ -28,11 +25,11 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
   const { callWithGasPrice } = useCallWithGasPrice()
   const { getBscScanURL } = useChainConfig()
   const launchpadContract = useAuctionSwapContract(pool)
-  const {loading,auctionSwapInfo,fetchPoolInfo} = useAuctionSwapInfo({pool})
+  const { loading, auctionSwapInfo, fetchPoolInfo } = useAuctionSwapInfo({ pool })
   const [accountBalance, setAccountBalance] = useState({})
   const [loadBalance, setLoadBalance] = useState(true)
   const [step, setStep] = useState(2)
-  const [tokenAddress,setTokenAddress] = useState(ethers.constants.AddressZero)
+  const [tokenAddress, setTokenAddress] = useState(ethers.constants.AddressZero)
   const [poolStatus, setPoolStatus] = useState("");
 
   useEffect(() => {
@@ -43,7 +40,7 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
     if (Date.parse(new Date()) < Date.parse(pool.open_date)) {
       setPoolStatus("coming")
     }
-    if (Date.parse(new Date()) > Date.parse(pool.end_date)){
+    if (Date.parse(new Date()) > Date.parse(pool.end_date)) {
       setPoolStatus("closed")
     }
     if (pool.open_date == null) {
@@ -51,7 +48,7 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
     }
   }, [])
 
-  const CountdownInPool = function(){
+  const CountdownInPool = function () {
     return (
       <div className={`flex text-base justify-between items-center"`}>
         {poolStatus == "open" && <div className="text-base">{t("Close in")}</div>}
@@ -64,7 +61,7 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
     )
   }
 
-  const reloadAccount = async function(){
+  const reloadAccount = async function () {
     await fetchPoolInfo()
     await fetchAccountBalance()
   }
@@ -86,32 +83,32 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
   useEffect(() => {
     if (loading) return false;
     setTokenAddress(auctionSwapInfo.info.addressItem)
-    if (auctionSwapInfo.info.isEnd){
-      if (auctionSwapInfo.order.totalWinItem > 0){
+    if (auctionSwapInfo.info.isEnd) {
+      if (auctionSwapInfo.order.totalWinItem > 0) {
 
       }
-      else{
-        if (auctionSwapInfo.order.total > 0){
+      else {
+        if (auctionSwapInfo.order.total > 0) {
           //place order success
           setStep(31)
         }
-        else{
+        else {
           //pool close
           setStep(32)
         }
       }
     }
-    else{
+    else {
       setStep(2)
     }
 
     //pool dont set winner
-  }, [loading,auctionSwapInfo]);
+  }, [loading, auctionSwapInfo]);
 
   useEffect(() => {
     if (step == 2) {
       store.step.update("2");
-    } else if (step == 3 ||step == 31 || step == 32 || step == 33 || step == 34 || step == 35) {
+    } else if (step == 3 || step == 31 || step == 32 || step == 33 || step == 34 || step == 35) {
       store.step.update("3");
     } else if (step == 4 || step == 41) {
       store.step.update("4");
@@ -132,30 +129,41 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
       {step == 2 &&
         <div className="project-main-actions no-padding overflow-hidden">
 
-          <div className="card-body no-padding">
+          <div className="card-body no-padding card card-default">
             <div className="flex flex-col">
               <div className="">
                 <Timeline step="2" />
+              </div>
+              
+              <div className="card-header items-end bg-primary-50 dark:bg-gray-900 rounded-t-lg">
+                <div className="flex flex-col">
+                  <h3 className="mb-2 font-medium">
+                    <span className="text-color-title">{t("Pool closes in")}</span>
+                  </h3>
+                  <ProjectCountdown project={project} pool={pool} isEndDate={true} />
+                </div>
+
+                <div className="text-center">
+                  <div className="">
+                    <div className="">
+                      <span className="mr-2 opacity-60">{t("Closeat")}</span> 
+                      <OpenDate time={pool.end_date} />
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <div className="project-card--container !p-0 mt-4">
                 <div className="flex">
                   <div className="w-full">
                     <div className="box-header relative flex !border-opacity-50">
-                      <h3>
+                      <h3 className="mb-2 font-medium">
                         Bid
                       </h3>
-                      {/* <div className="ml-auto flex !text-sm items-center">
-                        <button className="btn btn-default !px-1 mr-2">
-                          <span className="w-4 spin-10 h-4 rounded-full border-2 border-gray-300 dark:border-gray-400 border-l-purple-500 dark:border-l-purple-600" />
-                        </button>
-                        <span className="mr-2 !font-normal">Your position:</span>
-                        <div className="ml-auto">
-                          <span className="font-semibold">8</span>
-                          <span className="opacity-50">/215</span>
-                        </div>
-                      </div> */}
+
+
                     </div>
+
                     <SwapTokensV2 auctionSwapInfo={auctionSwapInfo} accountBalance={accountBalance} fetchAccountBalance={reloadAccount} setStep={setStep} project={project} pool={pool} />
                   </div>
 
@@ -184,35 +192,35 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
                             rounded-lg mb-4
                             border border-gray-200 dark:border-gray-700"
                         >
-                          {!!pool.end_date && 
-                          <div  className="py-1 px-4">
-                            <span className="mr-2 opacity-70">{t("Closeat")}</span> 
-                            <OpenDate time={pool.end_date} />
-                          </div>
+                          {!!pool.end_date &&
+                            <div className="py-1 px-4">
+                              <span className="mr-2 opacity-70">{t("Closeat")}</span>
+                              <OpenDate time={pool.end_date} />
+                            </div>
                           }
-                          {!!pool.whitelist_date && 
-                          <div  className="py-1 px-4">
-                            <span className="mr-2 opacity-70">{t("Announcement")}</span> 
-                            <OpenDate time={pool.whitelist_date} />
-                          </div>
+                          {!!pool.whitelist_date &&
+                            <div className="py-1 px-4">
+                              <span className="mr-2 opacity-70">{t("Announcement")}</span>
+                              <OpenDate time={pool.whitelist_date} />
+                            </div>
                           }
                         </div>
                       </div>
 
                       {!auctionSwapInfo.info.isEnd &&
-                      <div className="w-full text-left p-4 bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 rounded-lg flex cursor-pointer items-center group" onClick={e => { setStep(2) }} >
-                        <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-900 rounded-full flex-shrink-0 mr-4 shadow transition-all">
-                          <i className="fa fa-money-bill"></i>
-                        </span>
-                        <div>
-                          <p className="mb-1 text-lg text-yellow-600 dark:text-yellow-400">{t("Adjust bid")}</p>
+                        <div className="w-full text-left p-4 bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 rounded-lg flex cursor-pointer items-center group" onClick={e => { setStep(2) }} >
+                          <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-900 rounded-full flex-shrink-0 mr-4 shadow transition-all">
+                            <i className="fa fa-money-bill"></i>
+                          </span>
+                          <div>
+                            <p className="mb-1 text-lg text-yellow-600 dark:text-yellow-400">{t("Adjust bid")}</p>
 
-                          <a href={`#`}  className="group">
-                            <span className="text-sm mr-1">{t("adjust note",{"orderBusd" : auctionSwapInfo.order.totalItem,"maxBusd" : auctionSwapInfo.info.maxBuyPerAddress})}</span>
-                            <span className="icon text-xs relative left-1 group-hover:left-2 transition-all"><i className="fas fa-angle-right"></i></span>
-                          </a>
+                            <a href={`#`} className="group">
+                              <span className="text-sm mr-1">{t("adjust note", { "orderBusd": auctionSwapInfo.order.totalItem, "maxBusd": auctionSwapInfo.info.maxBuyPerAddress })}</span>
+                              <span className="icon text-xs relative left-1 group-hover:left-2 transition-all"><i className="fas fa-angle-right"></i></span>
+                            </a>
+                          </div>
                         </div>
-                      </div>
                       }
                     </div>
                   </div>
@@ -248,7 +256,7 @@ const SubscribeSwapToken = ({ project ,openTime,endTime,currentTime,pool}) => {
   );
 }
 
-const SubscribeSwapTokenLoading = function({currentTime,opendTime,endTime}){
+const SubscribeSwapTokenLoading = function ({ currentTime, opendTime, endTime }) {
   return (
     <div className="card-default project-main-actions no-padding overflow-hidden">
 
@@ -271,47 +279,51 @@ const SubscribeSwapTokenLoading = function({currentTime,opendTime,endTime}){
   )
 }
 
-const TokenSocialPromote = function({project}){
-  const {t,i18n} = useTranslation("launchpad")
+const TokenSocialPromote = function ({ project }) {
+  const { t, i18n } = useTranslation("launchpad")
   return (
     <ul className="text-left p-4 border border-gray-200 dark:border-gray-700 rounded-lg mb-4">
-      <li  className="relative pl-6  mb-2">
+      <li className="relative pl-6  mb-2">
         <span className="absolute left-0 top-0 mr-2">
           <i className="fa-duotone fa-circle-small opacity-60 mx-auto" />
         </span>
-        <p className="" dangerouslySetInnerHTML={{__html : t("status note")}} >
+        <p className="" dangerouslySetInnerHTML={{ __html: t("status note") }} >
         </p>
       </li>
       <li className="relative pl-6 mb-2">
         <span className="absolute left-0 top-0 mr-2">
           <i className="fa-duotone fa-circle-small opacity-60 mx-auto" />
         </span>
-          <p className="" dangerouslySetInnerHTML={{__html : t("coming soon note",
-          {
-            twitter : `<a class="link" target="_blank" rel="nofollow" href="https://twitter.com/rada_network">@rada_network</a>`,
-            radanetwork : `<a class="link" target="_blank" rel="nofollow" href="https://t.me/radanetwork">Telegram channel</a>`,
-            radadao : `<a class="link" target="_blank" rel="nofollow" href="https://t.me/radadao">Telegram Community</a>`
-          }
-          )}} />
+        <p className="" dangerouslySetInnerHTML={{
+          __html: t("coming soon note",
+            {
+              twitter: `<a class="link" target="_blank" rel="nofollow" href="https://twitter.com/rada_network">@rada_network</a>`,
+              radanetwork: `<a class="link" target="_blank" rel="nofollow" href="https://t.me/radanetwork">Telegram channel</a>`,
+              radadao: `<a class="link" target="_blank" rel="nofollow" href="https://t.me/radadao">Telegram Community</a>`
+            }
+          )
+        }} />
       </li>
       <li className="relative pl-6  mb-2">
         <span className="absolute left-0 top-0  mr-2">
           <i className="fa-duotone fa-circle-small opacity-60 mx-auto" />
         </span>
-          <p className="" dangerouslySetInnerHTML={{__html : t("status note 2",
+        <p className="" dangerouslySetInnerHTML={{
+          __html: t("status note 2",
             {
-              token : project.token.name,
-              research : `<a class="link" target="_blank" rel="nofollow" href="/${i18n.language}/launchverse/${project.slug}/research">Research</a>`
+              token: project.token.name,
+              research: `<a class="link" target="_blank" rel="nofollow" href="/${i18n.language}/launchverse/${project.slug}/research">Research</a>`
             }
-          )}} />
+          )
+        }} />
       </li>
 
     </ul>
   )
 }
 
-const NotInWhitesist = function(){
-  const {t} = useTranslation("launchpad")
+const NotInWhitesist = function () {
+  const { t } = useTranslation("launchpad")
   return (
     <div className="card-default project-main-actions no-padding overflow-hidden">
 
