891bbfb47f234f794ef91a582634c370752febeb
Hieu Nguyen <nnth83@gmail.com>
2022-02-27 02:25:40 +0700

    Update TutorialWidget.js

--- components/project/Item/Launchpad/TutorialWidget.js
+++ components/project/Item/Launchpad/TutorialWidget.js
@@ -31,9 +31,9 @@ const TutorialWidget = function({project,pool}){
           </Link>
           }
           {pool.project_pool_content && 
-          <div className="w-full p-4 my-2 bg-gray-100 dark:bg-gray-800 rounded-lg flex cursor-pointer items-center group border border-gray-200 dark:border-gray-700 hover:border-primary-500 hover:bg-white dark:hover:bg-gray-700 dark:hover:border-primary-500">
-            <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-900 rounded-full flex-shrink-0 mr-4 shadow">
-              <i className="fa-duotone fa-hand-holding-heart"></i>
+          <div className="w-full p-4 my-2 bg-gray-100 dark:bg-gray-900 rounded-lg flex cursor-pointer items-center group border border-gray-200 dark:border-gray-700 hover:border-primary-500 hover:bg-white dark:hover:bg-gray-800 dark:hover:border-primary-500">
+            <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-700 rounded-full flex-shrink-0 mr-4 shadow">
+              <i className="fa-duotone fa-info"></i>
             </span>
             <div>
               <p className="">{pool.project_pool_content.whitelist_title}</p>
@@ -44,9 +44,9 @@ const TutorialWidget = function({project,pool}){
             </div>
           </div>
           }
-          <div className="w-full p-4 my-2 bg-gray-100 dark:bg-gray-800 rounded-lg flex cursor-pointer items-center group border border-gray-200 dark:border-gray-700 hover:border-primary-500 hover:bg-white dark:hover:bg-gray-700 dark:hover:border-primary-500">
-            <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-900 rounded-full flex-shrink-0 mr-4 shadow">
-              <i className="fad fa-info"></i>
+          <div className="w-full p-4 my-2 bg-gray-100 dark:bg-gray-900 rounded-lg flex cursor-pointer items-center group border border-gray-200 dark:border-gray-700 hover:border-primary-500 hover:bg-white dark:hover:bg-gray-800 dark:hover:border-primary-500">
+            <span className="icon text-xl opacity-70 w-10 h-10 !flex items-center justify-center bg-white dark:bg-gray-700 rounded-full flex-shrink-0 mr-4 shadow">
+              <i className="fa-duotone fa-info"></i>
             </span>
             <div>
               <p className="">{t("How to join RADA LaunchVerse")}</p>
